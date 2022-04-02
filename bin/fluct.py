3791a8ef5fc10ce82393bc7c298abad636012932
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-04-02 02:04:51 +0200

    Update

--- bin/fluct.py
+++ bin/fluct.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 ftype = 'scatter'
-c_name_roots = ('ChooseGrainBD', 'MimicGrainBD', 'a2SeenBD', 'wBD')
-titles = ('Fluctuation of sensitivity for\ncomparing group mates', 'Fluctuation of sensitivity for\ncomparing partner to self', 'Fluctuation of help', 'Fluctuation of fitness')
+c_name_roots = ('ChooseGrainBD', 'MimicGrainBD')
+titles = ('Fluctuation of sensitivity for\ncomparing group mates', 'Fluctuation of sensitivity for\ncomparing partner to self')
 x_axis = 'MimicCost'
-x_label = 'Cost of comparing partner to self'
+x_label = ' \nCost of comparing partner to self'
 y_axis = 'ChooseCost'
-y_label = 'Cost of comparing group mates'
+y_label = 'Cost of comparing potential partners'
 x_min = None
 x_max = None
 y_min = None
 y_max = None
 bubble_size = 2000.0
-height = 10.0
-width = 20.8
+width = 12.0
+height = 5.8
 log = True
 movie = False

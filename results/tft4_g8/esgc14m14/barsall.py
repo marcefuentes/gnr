480d24bf67674e75bd405da95128dd9a89dafb6f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-06 23:41:59 +0200

    Update

--- results/tft4_g8/esgc14m14/barsall.py
+++ results/tft4_g8/esgc14m14/barsall.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-filename = 'grain'
-ftype = 'scatter' # barsall, barsone, scatter
+filename = 'grainbarsall'
+ftype = 'barsall' # barsall, barsone, scatter
 treatment = 'pr'
 control = 'none'
 drift = False
 movie = False
 x = 'ES'
 y = 'Given'
-z0 = 'ChooseGrainmedian'
-z1 = 'MimicGrainmedian'
+z0 = 'ChooseGrain'
+z1 = 'MimicGrain'
 
 if ftype == 'barsone':
     x_value = -1.0

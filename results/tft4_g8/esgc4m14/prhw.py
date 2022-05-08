047bd42140aa20861d01d8cfc6f2bc015d4c0e12
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-08 09:16:39 +0200

    Update

---
+++ results/tft4_g8/esgc4m14/prhw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+filename = 'prhw'
+ftype = 'scatter' # barsall, barsone, scatter
+treatment = 'pr'
+control = 'none'
+drift = False
+movie = False
+x = 'ES'
+y = 'Given'
+z0 = 'helpmedian'
+z1 = 'wmedian'
+
+if ftype == 'barsone':
+    x_value = -1.0
+    y_value = 1.0

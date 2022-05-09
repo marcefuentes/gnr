20e58bd8e8f103033558021a0e0eaecaf3371385
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-09 13:45:43 +0200

    Update

---
+++ results/tft7_g4/esgc14m14/barsall.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+filename = 'barsall'
+ftype = 'barsall' # barsall, barsone, scatter
+treatment = 'pr'
+control = 'none'
+drift = False
+movie = False
+x = 'ES'
+y = 'Given'
+z0 = 'help'
+z1 = 'w'
+
+if ftype == 'barsone':
+    x_value = -1.0
+    y_value = 1.0

322bb3f37b5be61c3a3722307193d1463604a2b8
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-18 11:49:44 +0200

    Update

---
+++ results/tft4_g8/nes5/rcmBR.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+filename = 'rcmBR'
+ftype = 'barsone' # barsall, barsone, scatter
+treatment = 'r'
+control = 'none'
+drift = False
+movie = False
+x = 'MimicCost'
+y = 'ChooseCost'
+z0 = 'ChooseGrain'
+z1 = 'MimicGrain'
+
+if ftype == 'barsone':
+    x_value = -7.0
+    y_value = -11.0

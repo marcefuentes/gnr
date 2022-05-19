126bf18212190650ee1cdcec7e612a9474d6c179
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-19 13:24:42 +0200

    Update

--- bin/addcolumn.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-import pandas as pd
-
-df = pd.read_csv(sys.argv[1])
-df.drop(sys.argv[2], inplace=True, axis=1)
-df[sys.argv[2]] = sys.argv[3]
-df.to_csv(sys.argv[1], index=False)

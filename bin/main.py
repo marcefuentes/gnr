6f25d11172c9cce4c1af47ec5b73d8f35f3eb692
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-24 18:27:28 +0200

    Update

--- bin/main.py
+++ bin/main.py
@@ -61,27 +61,15 @@ df_xy = pd.DataFrame(lst_xy, columns = ['xy', 'label', 'log', 'xymin', 'xymax'])
 df_z = pd.DataFrame(lst_z, columns = ['z', 'title', 'bubble_size', 'ymax'])
 
 x_name = module.x
-x_label = df_xy.loc[df_xy.xy == x_name, 'label'].values[0]
-if pd.isnull(df_xy.loc[df_xy.xy == x_name, 'xymin'].values[0]):
-    x_min = None
-else:
-    x_min = df_xy.loc[df_xy.xy == x_name, 'xymin'].values[0]
-if pd.isnull(df_xy.loc[df_xy.xy == x_name, 'xymax'].values[0]):
-    x_max = None
-else:
-    x_max = df_xy.loc[df_xy.xy == x_name, 'xymax'].values[0]
-x_log = df_xy.loc[df_xy.xy == x_name, 'log'].values[0]
 y_name = module.y
+x_label = df_xy.loc[df_xy.xy == x_name, 'label'].values[0]
 y_label = df_xy.loc[df_xy.xy == y_name, 'label'].values[0]
-if pd.isnull(df_xy.loc[df_xy.xy == y_name, 'xymin'].values[0]):
-    y_min = None
-else:
-    y_min = df_xy.loc[df_xy.xy == y_name, 'xymin'].values[0]
-if pd.isnull(df_xy.loc[df_xy.xy == y_name, 'xymax'].values[0]):
-    y_max = None
-else:
-    y_max = df_xy.loc[df_xy.xy == y_name, 'xymax'].values[0]
+x_log = df_xy.loc[df_xy.xy == x_name, 'log'].values[0]
 y_log = df_xy.loc[df_xy.xy == y_name, 'log'].values[0]
+x_min = None if pd.isnull(df_xy.loc[df_xy.xy == x_name, 'xymin'].values[0]) else df_xy.loc[df_xy.xy == x_name, 'xymin'].values[0]
+y_min = None if pd.isnull(df_xy.loc[df_xy.xy == y_name, 'xymin'].values[0]) else df_xy.loc[df_xy.xy == y_name, 'xymin'].values[0]
+x_max = None if pd.isnull(df_xy.loc[df_xy.xy == x_name, 'xymax'].values[0]) else df_xy.loc[df_xy.xy == x_name, 'xymax'].values[0]
+y_max = None if pd.isnull(df_xy.loc[df_xy.xy == y_name, 'xymax'].values[0]) else df_xy.loc[df_xy.xy == y_name, 'xymax'].values[0]
 z_names = [module.z0, module.z1]
 
 titles = []
@@ -102,10 +90,7 @@ class BarsAll:
         self.inner_cols = dfs[0][x_name].unique()
         self.inner_rows = dfs[0][y_name].unique()
         self.inner_cols.sort()
-        if y_name == 'GroupSize':
-            self.inner_rows.sort()
-        else:
-            self.inner_rows[::-1].sort()
+        self.inner_rows.sort() if y_name == 'GroupSize' else self.inner_rows[::-1].sort()
 
         self.bins = [(x+1)/bincount for x in range(bincount)]
         self.barwidth = 2.0/bincount
@@ -126,8 +111,7 @@ class BarsAll:
         fig.supxlabel(x_label, fontsize=fslabel)
         fig.supylabel(t=y_label, x=0.003*width, fontsize=fslabel, ha='center')
 
-        if module.movie:
-            fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
+        if module.movie: fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
 
         outer_grid = fig.add_gridspec(nrows=1, ncols=len(z_names), wspace=0.1)
 
@@ -138,15 +122,10 @@ class BarsAll:
             for row, (rowax, inner_row) in enumerate(zip(axs, self.inner_rows)): 
                 for column, (ax, inner_col) in enumerate(zip(rowax, self.inner_cols)):
                     medians = []
-                    for df in dfts:
-                        medians.append(df.loc[(df[x_name] == inner_col) & (df[y_name] == inner_row), z_name + 'median'].values[0])
+                    [medians.append(df.loc[(df[x_name] == inner_col) & (df[y_name] == inner_row), z_name + 'median'].values[0]) for df in dfts]
                     dif = medians[0] - medians[1]
-                    if ('Grain' in z_name) or ('BD' in z_name):
-                        dif = -dif
-                    if dif > 0.0:
-                        self.colors[0] = self.color_green
-                    else:
-                        self.colors[0] = self.color_blue
+                    if ('Grain' in z_name) or ('BD' in z_name): dif = -dif
+                    self.colors[0] = self.color_green if dif > 0.0 else self.color_blue
 
                     for df, color, alpha in zip(dfts, self.colors, self.alphas):
                         for b, name0, name1 in zip(self.bins[::2], z_namebins[::2], z_namebins[1::2]):
@@ -154,19 +133,12 @@ class BarsAll:
                             ax.bar(x=b, height=barheight, align='edge', color=color, linewidth=0, width=self.barwidth, alpha=alpha)
                             ax.set(xticks=[], yticks=[], ylim=[0, bh_max*2])
                     if (n == 0) & (column == 0):
-                        if y_log:
-                            y = '$2^{{{}}}$'.format(round(math.log(inner_row, 2)))
-                        else:
-                            y = inner_row
+                        y = '$2^{{{}}}$'.format(round(math.log(inner_row, 2))) if y_log else inner_row
                         ax.set_ylabel(y, rotation='horizontal', horizontalalignment='right', verticalalignment='center')
                     if row == len(self.inner_rows) - 1:
-                        if x_log:
-                            x = '$2^{{{}}}$'.format(round(math.log(inner_col, 2)))
-                        else:
-                            x = inner_col
+                        x = '$2^{{{}}}$'.format(round(math.log(inner_col, 2))) if x_log else inner_col
                         ax.set_xlabel(x)
-                    if column == len(self.inner_cols)/2:
-                        ax.set_title(title, fontsize=fstitle)
+                    if column == len(self.inner_cols)/2: ax.set_title(title, fontsize=fstitle)
 
         plt.savefig(outfile, dpi=100)
         plt.close()
@@ -175,14 +147,8 @@ class BarsOne:
 
     def prepare(self, dfs):
 
-        if x_log:
-            self.x_value = float(str("{:.6f}".format(pow(2, int(module.x_value)))))
-        else:
-            self.x_value = module.x_value
-        if y_log:
-            self.y_value = float(str("{:.6f}".format(pow(2, int(module.y_value)))))
-        else:
-            self.y_value = module.y_value
+        self.x_value = float(str("{:.6f}".format(pow(2, int(module.x_value))))) if x_log else module.x_value
+        self.y_value = float(str("{:.6f}".format(pow(2, int(module.y_value))))) if y_log else module.y_value
 
         bincount = int(sum(map(lambda x: z_names[0] in x, [*dfs[0]]))/2) - 2
 
@@ -195,10 +161,7 @@ class BarsOne:
             self.z_namebins_lists.append([z_name + str(x) for x in range(bincount)])
             self.z_namesdbins_lists.append([z_name + 'SD' + str(x) for x in range(bincount)])
             self.bh_maxs.append(df_z.loc[df_z.z == z_name, 'ymax'].values[0])
-            if z_name == 'w':
-                mmax = 2.0 # For a1Max = a2Max = 1.0 and R1 = R2 = 2.0.
-            else:
-                mmax = 1.0
+            mmax = 2.0 if z_name == 'w' else 1.0 # For a1Max = a2Max = 1.0 and R1 = R2 = 2.0.
             self.binslists.append([(x+1)*mmax/bincount for x in range(bincount)])
             self.barwidths.append(-mmax/bincount)
 
@@ -221,8 +184,7 @@ class BarsOne:
 
         fig.supylabel('\nFrequency', fontsize=fslabel, ha='center')
 
-        if module.movie:
-            fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
+        if module.movie: fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
 
         [ax.set_xlabel(title, fontsize=fslabel) for ax, title in zip(axs, titles)]
 
@@ -230,14 +192,9 @@ class BarsOne:
             medians = []
             [medians.append(df.loc[(df[x_name] == self.x_value) & (df[y_name] == self.y_value), z_name + 'median'].values[0]) for df in dfts]
             dif = medians[0] - medians[1]
-            if ('Grain' in z_name) or ('BD' in z_name):
-                dif = -dif
-            if dif > 0.0:
-                self.colors[0] = self.color_green
-                self.colorsds[0] = self.colorsd_green
-            else:
-                self.colors[0] = self.color_blue
-                self.colorsds[0] = self.colorsd_blue
+            if ('Grain' in z_name) or ('BD' in z_name): dif = -dif
+            self.colors[0] = self.color_green if dif > 0.0 else self.color_blue
+            self.colorsds[0] = self.color_green if dif > 0.0 else self.colorsd_blue
             for df, color, colorsd, alpha in zip(dfts, self.colors, self.colorsds, self.alphas):
                 for b, z_namebin, z_namesdbin in zip(bins, z_namebins, z_namesdbins):
                     barheight = df.loc[(df[x_name] == self.x_value) & (df[y_name] == self.y_value), z_namebin]
@@ -247,8 +204,7 @@ class BarsOne:
                 ax.set(ylim=(0, bh_max), yticks=(0, bh_max), yticklabels=(0, bh_max))
                 ax.tick_params(axis='x', labelsize=fstick)
                 ax.tick_params(axis='y', labelsize=fstick)
-                if z_name != z_names[0]:
-                    ax.set(yticks=[])
+                if z_name != z_names[0]: ax.set(yticks=[])
                 ax.set_box_aspect(1)
 
         plt.savefig(outfile, dpi=100)
@@ -270,8 +226,7 @@ class Scatter:
         fig.supxlabel(t=x_label, y=0.02, fontsize=fslabel)
         fig.supylabel(t=y_label, x=0.04, fontsize=fslabel, ha='center')
 
-        if module.movie:
-            fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
+        if module.movie: fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
 
         [dft.sort_values(by=[x_name, y_name], inplace=True) for dft in dfts]
 
@@ -279,36 +234,29 @@ class Scatter:
             ax.set_title(title, pad=10.0, fontsize=fstitle)
             ax.tick_params(axis='x', labelsize=fstick)
             ax.tick_params(axis='y', labelsize=fstick)
-            if x_log:
-                ax.set_xscale('log', base=2)
-            if y_log:
-                ax.set_yscale('log', base=2)
+            if x_log: ax.set_xscale('log', base=2)
+            if y_log: ax.set_yscale('log', base=2)
             dif = dfts[1][z_name] - dfts[0][z_name]
-            if (z_name == 'ChooseGrainmedian') or (z_name == 'MimicGrainmedian') or ('BD' in z_name):
-                dif = -dif
+            if (z_name == 'ChooseGrainmedian') or (z_name == 'MimicGrainmedian') or ('BD' in z_name): dif = -dif
             color = []
             for i in dif:
                 if i < 0.0:
-                    if i < -red:
-                        i = -red
+                    if i < -red: i = -red
                     color.append((red + i, green + i/2.0, blue + i))
                 else:
-                    if i > red:
-                        i = red
+                    if i > red: i = red
                     color.append((red - i, green - i, blue - i/2.0))
             for suffix, suffixec, suffixalpha in zip(self.suffixes, self.suffixecs, self.suffixalphas):
                 df = dfts[module.drift]
                 x = df[x_name]
                 y = df[y_name]
                 s = df[z_name]
-                if suffix == 'SD':
-                    s = s + df[z_name + suffix]
+                if suffix == 'SD': s = s + df[z_name + suffix]
                 if module.drift:
                     ax.scatter(x, y, color='0.700', edgecolor='0.700', alpha=suffixalpha, s=s*bubble_size)
                 else:
                     ax.scatter(x, y, c=color, ec=color, alpha=suffixalpha, s=s*bubble_size)
-                if z_name != z_names[0]:
-                    ax.axes.yaxis.set_ticklabels([])
+                if z_name != z_names[0]: ax.axes.yaxis.set_ticklabels([])
                 ax.set_xlim(x_min, x_max)
                 ax.set_ylim(y_min, y_max)
                 ax.set_box_aspect(1)
@@ -317,18 +265,12 @@ class Scatter:
         plt.close()
 
 def get_data(dfs):
-
     dirs = [module.treatment, module.control]
-
     for d in dirs:
         df = pd.concat(map(pd.read_csv, glob.glob(os.path.join(d, '*.csv'))), ignore_index=True)
         dfs.append(df)
-
-    if module.control == 'optimal':
-        dfs[1]['helpmedian'] = dfs[1]['a2Seenmedian']*2.0*dfs[1]['Given']
-    if module.treatment == 'optimal':
-        dfs[0]['helpmedian'] = dfs[0]['a2Seenmedian']*2.0*dfs[0]['Given']
-
+    if module.control == 'optimal': dfs[1]['helpmedian'] = dfs[1]['a2Seenmedian']*2.0*dfs[1]['Given']
+    if module.treatment == 'optimal': dfs[0]['helpmedian'] = dfs[0]['a2Seenmedian']*2.0*dfs[0]['Given']
     return dfs
 
 def create_figure(t):

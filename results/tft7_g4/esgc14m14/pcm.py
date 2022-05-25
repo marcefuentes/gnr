2c1912780ec0b3ffe3d7eb65244fc8f953d4f359
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-05-25 08:39:49 +0200

    Update

--- results/tft7_g4/esgc14m14/pcm.py
+++ results/tft7_g4/esgc14m14/pcm.py
@@ -2,14 +2,17 @@
 
 filename = 'pcm'
 ftype = 'scatter' # barsall, barsone, scatter
-treatment = 'p'
-control = 'none'
 drift = False
 movie = False
 x = 'ES'
 y = 'Given'
-z0 = 'ChooseGrainmedian'
-z1 = 'MimicGrainmedian'
+z0 = {}
+z0['name'] = 'ChooseGrainmedian'
+z0['treatment'] = 'p'
+z0['control'] = 'none'
+z1['name'] = 'MimicGrainmedian'
+z1['treatment'] = 'p'
+z1['control'] = 'none'
 
 if ftype == 'barsone':
     x_value = -1.0

d4ffc2e1de1450630d47a26908488ef62e0b4253
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-06-01 16:04:55 +0200

    Update

--- bin/main.py
+++ bin/main.py
@@ -201,8 +201,7 @@ class Scatter:
 
         if module.movie: fig.text(0.93, 0.02, f'Time = {t}', fontsize=14, color='grey', ha='right')
 
-        for d in module.dirs:
-            dfts[d].sort_values(by=[xname, yname], inplace=True)
+        [dfts[d].sort_values(by=[xname, yname], inplace=True) for d in module.dirs]
 
         for row, (rowax, zdicts) in enumerate(zip(axs, zdictss)):
             for ax, zdict in zip(rowax, zdicts):

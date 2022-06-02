e2d25a97226ceb0a85c8af43d55faeff9611375d
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-06-02 19:44:12 +0200

    Update

---
+++ results/tft7_g16/esgc14m14/pyr3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+filename = 'pyr3'
+ftype = 'scatter' # barsall, barsone, scatter
+movie = False
+x = 'ES'
+y = 'Given'
+
+z0 = {}
+z0['name'] = 'ChooseGrainmedian'
+z0['treatment'] = 'p'
+z0['control'] = 'none'
+
+z1 = {}
+z1['name'] = 'wmedian'
+z1['treatment'] = 'p'
+z1['control'] = 'none'
+
+z2 = {}
+z2['name'] = 'wmedian'
+z2['treatment'] = 'p'
+z2['control'] = 'optimal'
+
+z02 = {}
+z02['name'] = 'MimicGrainmedian'
+z02['treatment'] = 'r'
+z02['control'] = 'none'
+
+z12 = {}
+z12['name'] = 'wmedian'
+z12['treatment'] = 'r'
+z12['control'] = 'none'
+
+z22 = {}
+z22['name'] = z2['name']
+z22['treatment'] = 'r'
+z22['control'] = 'optimal'
+
+z = [[z0, z1, z2], [z02, z12, z22]]
+
+dirs = ['none', 'optimal', 'p', 'r']
+
+if ftype == 'barsone':
+    x_value = -1.0
+    y_value = 1.0
+

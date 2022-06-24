be0fa5f7c1d5e4cdd5d8cc1d46d2adedd2e538fe
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-06-24 21:15:34 +0200

    Update

--- bin/rmap.py
+++ bin/rmap.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Parameters
 
 alpha = 0.5
-ES = -1.0
+ES = pow(2, -5)
 R1 = 2.0
 R2 = 2.0
 given = 1.0
@@ -30,14 +30,14 @@ plt.figure()
 
 for ai in ais:
     y0 = fitness(R1*(1.0-ais), R2*(ais*(1.0-given) + ai*given))
-    plt.plot(ais, y0, c='yellow', linewidth=ai)
-    yr = fitness(R1*(1.0-ais), R2*(ais*(1.0-given) + ais*given))
+    plt.plot(ais, y0, c='yellow', linewidth=ai*2.0)
+    yr = fitness(R1*(1.0-ai), R2*(ai*(1.0-given) + ai*given))
     y = (y0 + yr*(repeats - 1.0))/repeats
-    plt.plot(ais, y, c='red')
+    plt.plot(ais, y, c='red', linewidth=ai*2.0)
     y = ((y0 + yr)/2.0)
-    plt.plot(ais, y, c='orange', linewidth=ai)
-    #ax.set_xlim(0.0, max_a2)
-    #ax.set_ylim(0.0, 1.5, 1)
+    #plt.plot(ais, y, c='orange', linewidth=ai*2.0)
+    plt.xlim(0.0, 1.0)
+    plt.ylim(0.0, 2.0)
     #ax.set_xticks(np.linspace(0.0, max_a2, num=3))
     #ax.set_yticks(np.linspace(0.0, 1.5, num=4))
     plt.xlabel('$\it{a_{2i}}$', fontsize=fs) 

1390f759ec92420b8c3e4216b62cf36e6a315e96
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-06-29 09:22:35 +0200

    Update

---
+++ results/q/esg/optimal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+filename = 'optimal'
+ftype = 'bubbles' # barsall, barsone, scatter
+movie = False
+
+glos = {'x': 'ES', 'y': 'Given'}
+traits = ['a2Seenmedian', 'helpmedian', 'wmedian']
+top_row = [{'treatment':'optimal', 'control': 'none'}, {'treatment':'optimal', 'control': 'none'}, {'treatment':'optimal', 'control': 'none'}]
+bottom_row = [{'treatment': 'none', 'control': 'optimal'}, {'treatment': 'none', 'control': 'optimal'}, {'treatment': 'none', 'control': 'optimal'}]
+rows = [top_row, bottom_row]
+
+if ftype == 'barsone': glovalue = {'x': -1.0, 'y': 1.0}

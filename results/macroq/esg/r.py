a04b2694d6d76b735cd762e419e294fac67a8253
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-07-06 18:59:37 +0200

    Update

--- results/macroq/esg/r.py
+++ results/macroq/esg/r.py
@@ -6,7 +6,7 @@ movie = False
 
 glos = {'x': 'alpha', 'y': 'Given'}
 traits = ['MimicGrain6', 'a2Seen51', 'wmedian']
-top_row = [{'treatment':'r', 'control': 'none'}, {'treatment':'r', 'control': 'none'}, {'treatment':'r', 'control': 'optimal'}]
+top_row = [{'treatment':'r', 'control': 'none'}, {'treatment':'r', 'control': 'none'}, {'treatment':'r', 'control': 'none'}]
 bottom_row = [{'treatment': 'none', 'control': 'optimal'}, {'treatment': 'none', 'control': 'optimal'}, {'treatment': 'none', 'control': 'optimal'}]
 rows = [top_row]
 

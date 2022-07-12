0189d9486832285a105275e744f3cff0b34aee5f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-07-12 12:28:02 +0200

    Update

--- bin/games.py
+++ bin/games.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+fs = 16     # Label font size
 
 # Parameters
 
 alpha = 0.5
 R1 = 2.0
 R2 = 2.0
-#deathrate = pow(2, -7)
-deathrate = 1.0
-width = 15
-height = 15
-
-n_data = 2
-fs = 14
 
 ess = np.linspace(-5, 5, num=11)
 ess = pow(2, ess)
 rhos = 1.0 - 1.0/ess
 givens = np.linspace(1.0, 0.0, num=11)
-aDs = np.linspace(0.1, 0.4, 4)
-aCs = np.linspace(0.2, 0.5, 4)
-colors = ('0.800', '0.600', '0.400', '0.200')
-xs = np.linspace(0.0, 1.0, num=n_data)
-zerodifs = [0.0] * n_data
-repeats = 1.0/(1.0 - pow(1.0 - deathrate, 2))
+aC = 0.5
+aD = 0.2
+q1C = R1*(1.0-aC);
+q1D = R1*(1.0-aD);
 
 def fitness(q1, q2, rho):
     if rho == 0.0:
@@ -34,35 +29,48 @@ def fitness(q1, q2, rho):
         w = pow(alpha*pow(q1, rho) + (1.0 - alpha)*pow(q2, rho), 1.0/rho)
     return w
 
-for aC, aD, color in zip(aCs, aDs, colors):
-    fig, axs = plt.subplots(11, 11, figsize=(12, 12))
-    q1C = R1*(1.0-aC);
-    q1D = R1*(1.0-aD);
-    for rowax, given in zip(axs, givens):
-        for ax, rho in zip(rowax, rhos):
-            wR = fitness(q1C, R2*aC, rho)
-            wT = fitness(q1D, R2*(aD*(1-given)+aC*given), rho)
-            wS = fitness(q1C, R2*(aC*(1-given)+aD*given), rho)
-            wP = fitness(q1D, R2*aD, rho)
-            wD = wT*xs + wP*(1-xs)
-            wC = wR*xs + wS*(1-xs)
-            #difs = wC - wD
-            #ax.plot(xs, zerodifs, c='orange')
-            #ax.plot(xs, difs, c=color)
-            ax.plot(xs, wD, c='orange')
-            ax.plot(xs, wC, c='green')
-            #ax.set_ylim(-0.1, 0.3)
-            ax.set_ylim(0.0, 1.3)
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_box_aspect(1)
-            #ax.set_xticks(np.linspace(0.0, max_a2, num=3))
-            #ax.set_yticks(np.linspace(0.0, 1.5, num=4))
-            #ax.xlabel('$\it{a_{2i}}$', fontsize=fs) 
-            #ax.ylabel('$\it{w}$', fontsize=fs)
+x = []
+y = []
+size0 = []
+size1 = []
+color0 = []
+color1 = []
 
-    plt.subplots_adjust(wspace=0, hspace=0)
+for rho in rhos:
+    R = fitness(q1C, R2*aC, rho)
+    P = fitness(q1D, R2*aD, rho)
+    for given in givens:
+        x.append([1/(1-rho)])
+        y.append(given)
+        T = fitness(q1D, R2*(aD*(1.0-given) + aC*given), rho)
+        S = fitness(q1C, R2*(aC*(1.0-given) + aD*given), rho)
+        wC0 = S
+        wD0 = P
+        wC1 = R
+        wD1 = T
+        diff0 = wC0 - wD0
+        if diff0 > 0:
+            color0.append('#59ff00') 
+        else:
+            color0.append('#ff8300')
+        size0.append(abs(diff0))
+        diff1 = wC1 - wD1
+        if diff1 > 0:
+            color1.append('#2d8000') 
+        else:
+            color1.append('#ff0000')
+        size1.append(abs(diff1))
 
-    plt.savefig(f'games{aC}.png')
+fig, ax = plt.subplots(figsize=(7,7))
+ax.scatter(x=x, y=y, s=1200.0*np.array(size0), color='none', ec=color0, alpha=0.5)
+ax.scatter(x=x, y=y, s=1200.0*np.array(size1), color='none', ec=color1, alpha=0.5)
+ax.set_xlabel('Substitutability of resource $\it{A}$', fontsize=fs+6)
+ax.set_ylabel('Partner\'s share of resource $\it{A}$', fontsize=fs+6)
+ax.tick_params(axis='x', labelsize=fs)
+ax.tick_params(axis='y', labelsize=fs)
+ax.set_xscale('log', base=2)
+ax.set_box_aspect(1)
 
-    plt.close()
+fig.savefig('games.png', transparent=False)
+plt.close()
+

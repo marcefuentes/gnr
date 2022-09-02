73293611728aac655c036120ae13e7bf40a25dd9
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-09-02 11:31:04 +0200

    Update

--- bin/tr.py
+++ bin/tr.py
@@ -22,9 +22,8 @@ shiftconstant = 0.7 # Macromutations
 alpha = 0.5
 R1 = 2.0
 R2 = 2.0
+mutationsize = 0.0078125*2
 
-n = 16
-aCs = np.linspace(0.5, 0.2, num=n)
 #log_ess = np.linspace(-5, 5, num=11) if sys.argv[1] == 'ces' else np.linspace(-10, 0, num=11)
 log_ess = np.linspace(-3, -3, num=1) if sys.argv[1] == 'ces' else np.linspace(-10, 0, num=11)
 ess = pow(2, log_ess)
@@ -52,34 +51,33 @@ for ax, title, letter in zip(axs, titles, letters):
     for es, log_es in zip(ess, log_ess):
         rho = 1.0 - 1.0/es if sys.argv[1] == 'ces' else es
         for given in givens:
-            for aC in aCs:
+            aC = mutationsize*2
+            while aC < 1.0:
                 q1C = R1*(1.0-aC);
                 R = wC1 = fitness(q1C, R2*aC, rho)
-                aDs = np.linspace(aC-0.1, 0.1, num=int(aC*10) - 1)
-                for aD in aDs:
+                aD = mutationsize
+                while aD < aC:
                     q1D = R1*(1.0-aD);
                     P = wD0 = fitness(q1D, R2*aD, rho)
-                    x = []
-                    y = []
                     T = wD1 = fitness(q1D, R2*(aD*(1.0-given) + aC*given), rho)
                     S = wC0 = fitness(q1C, R2*(aC*(1.0-given) + aD*given), rho)
                     if (T<R) and (P<S):
-                        # rgb = (0.5-shift, 0.5+shift, 0.5+shift)
-                        rgb = (0.5, 0.0, 1.0)
+                        rgb = (0.5, 0.0, 1.0) # No dilemma
                     else:
                         if (T>R) and (P<S):
                             rgb = (0.0, 1.0, 0.0) # Snowdrift
                         else:
-                            rgb = (1.0, 0.0, 0.5)
-                            # rgb = (0.5-shift, 0.5+shift, 0.5+shift)
+                            rgb = (1.0, 0.0, 0.5) # Prisoner's dilemma
                     ax.plot(aC, aD, color=rgb, marker='o', markerfacecolor=rgb, markersize=3)
-                    ax.set_title(title, pad=10.0, fontsize=fstitle)
-                    ax.tick_params(axis='x', labelsize=fstick)
-                    ax.tick_params(axis='y', labelsize=fstick)
-                    ax.set_xlim(0.0, 1.0)
-                    ax.set_ylim(0.0, 1.0)
-                    ax.set_box_aspect(1)
-                    ax.text(0.01, 1.1, letter, fontsize=18, weight='bold')
+                    #ax.set_title(title, pad=10.0, fontsize=fstitle)
+                    #ax.tick_params(axis='x', labelsize=fstick)
+                    #ax.tick_params(axis='y', labelsize=fstick)
+                    #ax.set_xlim(0.0, 1.0)
+                    #ax.set_ylim(0.0, 1.0)
+                    #ax.set_box_aspect(1)
+                    #ax.text(0.01, 1.1, letter, fontsize=18, weight='bold')
+                    aD += mutationsize
+                aC += mutationsize
 
 fig.savefig(filename, transparent=False)
 plt.close()

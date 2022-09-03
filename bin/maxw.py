02a70b982560ce9c7401e0fb2d526049503d6f72
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-09-03 18:01:03 +0200

    Update

--- bin/maxw.py
+++ bin/maxw.py
@@ -11,8 +11,8 @@ plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
 height = 6.0
-fslabel = 40
-fstitle= 30
+fslabel = 48
+fsnumbers= 24
 
 # Parameters
 
@@ -24,9 +24,13 @@ mutationsize = 0.0078125
 log_ess = np.linspace(-5, 5, num=11) if sys.argv[1] == 'ces' else np.linspace(-10, 0, num=11)
 ess = pow(2, log_ess)
 givens = np.linspace(1.0, 0.0, num=11)
-aXs = aYs = np.linspace(mutationsize, 1.0 - mutationsize)
+x = np.linspace(mutationsize, 1.0 - mutationsize, num = 32)
+y = np.linspace(1.0 - mutationsize, mutationsize, num = 32)
+X, Y = np.meshgrid(x, y)
 
-def fitness(q1, q2, rho):
+def fitness(x, y, given, rho):
+    q1 = (1-y)*R1
+    q2 = y*R2*(1-given) + x*R2*given
     if sys.argv[1] == 'q':
         w = 4.0*pow(q1, rho)/9.0 + 4.0*q2/9.0
     else:
@@ -43,22 +47,16 @@ fig.supylabel('Partner\'s share of $\it{A}$', fontsize=fslabel)
 for row, given in zip(axs, givens):
     for ax, es, log_es in zip(row, ess, log_ess):
         rho = 1.0 - 1.0/es if sys.argv[1] == 'ces' else es
-        for aX in aXs:
-            q1X = R1*(1.0-aX)
-            for aY in aYs:
-                q1Y = R1*(1.0-aY)
-                T = fitness(q1Y, R2*(aY*(1.0-given) + aX*given), rho)
-                rgb = (T/2.0, T/2.0, T/2.0) 
-                ax.plot(aX, aY, color=rgb, marker='s', markerfacecolor=rgb, markersize=3.0)
-                ax.set(xticks=[], yticks=[], xlim=[0.0, 1.0], ylim=[0.0, 1.0])
-                #ax.set_box_aspect(1)
-                #ax.text(0.01, 1.1, letter, fontsize=18, weight='bold')
+        Z = fitness(X, Y, given, rho)
+        ax.imshow(Z, vmin=0, vmax=2)
+        ax.set(xticks=[], yticks=[])
+        #ax.set_box_aspect(1)
+        #ax.text(0.01, 1.1, letter, fontsize=18, weight='bold')
         if given == 0.0:
-            x = '$2^{{{}}}$'.format(log_es)
-            ax.set_xlabel(x, fontsize=fslabel)
+            x = '$2^{{{}}}$'.format(round(log_es))
+            ax.set_xlabel(x, fontsize=fsnumbers)
         if log_es == -5:
-            print(given)
-            ax.set_ylabel(given, rotation='horizontal', horizontalalignment='right', verticalalignment='center', fontsize=fslabel)
+            ax.set_ylabel(round(given, 1), rotation='horizontal', horizontalalignment='right', verticalalignment='center', fontsize=fsnumbers)
 
 fig.savefig(filename, transparent=False)
 plt.close()

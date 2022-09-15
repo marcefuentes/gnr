24438740808c297de761dc1e302defe72ad8ad10
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-09-15 10:56:46 +0200

    Update

--- bin/coop.py
+++ bin/coop.py
@@ -17,32 +17,25 @@ letters = [['a', 'b', 'c', 'd', 'e'],
             ['f', 'g', 'h', 'i', 'j'],
             ['k', 'l', 'm', 'n', 'o'],
             ['p', 'q', 'r', 's', 't'],
-            ['u', 'v', 'w', 'x', 'y']]
+            ['u', 'v', 'w', 'x', 'y'],
+            ['z', 'aa', 'ab', 'ac', 'ad']]
 
 traits = ['a2Seenmedian', 'helpmedian', 'wmedian', 'ChooseGrainmedian', 'MimicGrainmedian']
 traitlabels = ['Effort to get $\it{A}$', 'Help', 'Fitness', 'Sensitivity for\nchoosing partner', 'Sensitivity for\nmimicking partner']
 traitvmaxs = [0.5, 1.0, 1.0, 1.0, 1.0]
-folders = ['none', 'p', 'r', 'pr']
+folders = ['none', 'p', 'r', 'pr', 'pr']
 
 alpha = 0.5
 R1 = 2.0
 R2 = 2.0
-R = R2/R1
 a1max = 1.0
 a2max = 1.0
-b = a2max/a1max
 
 num = 2000
 minlog_es = -5.0
 maxlog_es = 5.0
 mingiven = 0.0
 maxgiven = 1.0
-
-log_ess = np.linspace(minlog_es, maxlog_es, num=num)
-rhos = 1.0 - 1.0/pow(2, log_ess)
-givens = np.linspace(maxgiven, mingiven, num=num)
-givens[0] = 0.99999
-Xrhos, Ygivens = np.meshgrid(rhos, givens)
 
 def a2eq(X, Y):
     T = b*R*(1.0 - Y)
@@ -56,6 +49,13 @@ def fitness(Z, X, Y):
     w = np.where(X == 0.0, pow(q1, alpha)*pow(q2, 1.0 - alpha), pow(alpha*pow(q1, X) + (1.0 - alpha)*pow(q2, X), 1.0/X)) 
     return w
 
+R = R2/R1
+b = a2max/a1max
+log_ess = np.linspace(minlog_es, maxlog_es, num=num)
+rhos = 1.0 - 1.0/pow(2, log_ess)
+givens = np.linspace(maxgiven, mingiven, num=num)
+givens[0] = 0.99999
+Xrhos, Ygivens = np.meshgrid(rhos, givens)
 Z0 = a2eq(Xrhos, Ygivens)
 Zs = [Z0, Z0*R2*Ygivens, fitness(Z0, Xrhos, Ygivens), np.ones([num, num])*0.1, np.ones([num, num])*0.1]
 Zmaxs = [0.5, 1.0, 1.0, 1.0, 1.0]
@@ -78,7 +78,7 @@ y = dfts[folders[0]]['Given']
 fslabel=36 # Label font size
 fstick=24 # Tick font size
 
-fig, axs = plt.subplots(nrows=5, ncols=5, figsize=(30, 30)) # constrained_layout=False, squeeze=False
+fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1))) # constrained_layout=False, squeeze=False
 
 fig.supxlabel('Substitutability of $\it{A}$', x=0.513, y=0.05, fontsize=fslabel*1.25)
 fig.supylabel('Partner\'s share of $\it{A}$', x=0.05, y=0.493, fontsize=fslabel*1.25, ha='center')
@@ -91,7 +91,7 @@ for ax, Z, Zmax, letter, traitlabel in zip(axs[0], Zs, Zmaxs, letters[0], traitl
     ax.set_yticks([0, num/2.0, num])
     ax.set_xticklabels([])
     ax.set_yticklabels([])
-    if 'Effort' in traitlabel:
+    if traitlabel == traitlabels[0]:
         ax.set_yticklabels([0.0, 0.5, 1.0], fontsize=fstick) 
     ax.text(0, 2070, letter, fontsize=fslabel, weight='bold')
 
@@ -106,13 +106,12 @@ for axrow, folder, letterrow in zip(axs[1:], folders, letters[1:]):
         ax.set_xticks([-5, 0, 5])
         ax.set_yticks([0.0, 0.5, 1.0])
         ax.set_xticklabels([])
-        if 'pr' in folder:
+        if folder == folders[-1]:
             ax.set_xticklabels([-5, 0, 5], fontsize=fstick)
         ax.set_yticklabels([])
-        if 'Effort' in traitlabel:
+        if traitlabel == traitlabels[0]:
             ax.set_yticklabels([0.0, 0.5, 1.0], fontsize=fstick) 
         ax.text(-5.5, 1.09, letter, fontsize=fslabel, weight='bold')
-        ax.set_box_aspect(1)
 
 plt.savefig('coop.png', transparent=False)
 plt.close()

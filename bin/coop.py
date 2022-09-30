9d65354f77bdcca713469dfd769292fd54e14869
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-09-30 17:50:50 +0200

    Update

--- bin/coop.py
+++ bin/coop.py
@@ -9,9 +9,6 @@ import time
 
 start_time = time.perf_counter ()
 
-plt.rcParams['pdf.fonttype'] = 42
-plt.rcParams['ps.fonttype'] = 42
-
 letters = [['a', 'b', 'c', 'd', 'e'],
             ['f', 'g', 'h', 'i', 'j'],
             ['k', 'l', 'm', 'n', 'o'],
@@ -29,7 +26,6 @@ R1 = 2.0
 R2 = 2.0
 a1max = 1.0
 a2max = 1.0
-
 num = 2000
 minlog_es = -5.0
 maxlog_es = 5.0
@@ -52,6 +48,18 @@ def a2eq(given):
     a2 = a2max/(1.0 + Q*b)
     return a2
     
+fslabel=36 # Label font size
+fstick=24 # Tick font size
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1))) # constrained_layout=False, squeeze=False
+
+fig.supxlabel('Substitutability of $\it{A}$', x=0.513, y=0.05, fontsize=fslabel*1.25)
+fig.supylabel('Partner\'s share of $\it{A}$', x=0.05, y=0.493, fontsize=fslabel*1.25, ha='center')
+
+# Top row of subplots
+
 R = R2/R1
 b = a2max/a1max
 log_ess = np.linspace(minlog_es, maxlog_es, num=num)
@@ -62,14 +70,6 @@ Xrhos, Ygivens = np.meshgrid(rhos, givens)
 Aeq = a2eq(Ygivens)
 Feq = fitness(Aeq, Aeq)
 Zs = [Aeq, Aeq*R2*Ygivens, Feq, np.ones([num, num])*0.1, np.ones([num, num])*0.1]
-
-fslabel=36 # Label font size
-fstick=24 # Tick font size
-
-fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1))) # constrained_layout=False, squeeze=False
-
-fig.supxlabel('Substitutability of $\it{A}$', x=0.513, y=0.05, fontsize=fslabel*1.25)
-fig.supylabel('Partner\'s share of $\it{A}$', x=0.05, y=0.493, fontsize=fslabel*1.25, ha='center')
 
 extent = 0, num, 0, num
 for ax, Z, traitvmax, letter, traitlabel in zip(axs[0], Zs, traitvmaxs, letters[0], traitlabels):
@@ -82,6 +82,8 @@ for ax, Z, traitvmax, letter, traitlabel in zip(axs[0], Zs, traitvmaxs, letters[
     if traitlabel == traitlabels[0]:
         ax.set_yticklabels([round(mingiven, 1), round((mingiven + maxgiven)/2, 1), round(maxgiven, 1)], fontsize=fstick) 
     ax.text(0, num*1.035, letter, fontsize=fslabel, weight='bold')
+
+# Remaining rows of plots
 
 dfs = {}
 for folder in folders:

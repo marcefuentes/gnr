4f200cb86058376884cc4ab72e041aa9c7fd04b1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-10-18 12:32:33 +0200

    Update

--- bin/discrete.py
+++ bin/discrete.py
@@ -17,7 +17,7 @@ letters = [['a', 'b', 'c', 'd', 'e'],
             ['u', 'v', 'w', 'x', 'y'],
             ['z', 'aa', 'ab', 'ac', 'ad']]
 
-#traits = ['a2Seenmedian', 'help', 'wmedian', 'ChooseGrainmedian', 'MimicGrainmedian']
+#traits = ['a2Seensd', 'help', 'wsd', 'ChooseGrainsd', 'MimicGrainsd']
 traits = ['a2Seenmean', 'help', 'wmean', 'ChooseGrainmean', 'MimicGrainmean']
 traitlabels = ['Effort to get $\it{A}$', 'Help', 'Fitness', 'Sensitivity for\nchoosing partner', 'Sensitivity for\nmimicking partner']
 traitvmaxs = [0.5, 1.0, 1.0, 0.6, 0.6]
@@ -29,10 +29,18 @@ R2 = 2.0
 a1max = 1.0
 a2max = 1.0
 
-def fitness(A, Apartner):
-    q1 = R1*(a2max - A)/b
-    q2 = R2*(A*(1.0 - Ygivens) + Apartner*Ygivens)
-    w = np.where(Xrhos == 0.0, pow(q1, alpha)*pow(q2, 1.0 - alpha), pow(alpha*pow(q1, Xrhos) + (1.0 - alpha)*pow(q2, Xrhos), 1.0/Xrhos)) 
+def fitness(x, y):
+    q1 = (1.0 - y)*R1
+    q2 = y*R2*(1.0 - GG) + x*R2*GG
+    w = q1*q2
+    mask = (w > 0.0) & (RR == 0.0)
+    w[mask] = pow(q1[mask], alpha)*pow(q2[mask], 1.0 - alpha)
+    mask = (w > 0.0) & (RR < 0.0)
+    w[mask] = alpha*pow(q1[mask], RR[mask]) + (1.0 - alpha)*pow(q2[mask], RR[mask])
+    mask = (w > 0.0) & (RR < 0.0)
+    w[mask] = pow(w[mask], 1.0/RR[mask])
+    mask = (RR > 0.0)
+    w[mask] = pow(alpha*pow(q1[mask], RR[mask]) + (1.0 - alpha)*pow(q2[mask], RR[mask]), 1.0/RR[mask])
     return w
 
 dfs = []
@@ -47,23 +55,30 @@ for df in dfs:
 for df in dfts:
     df['help'] = df[traits[0]]*R2*df.Given
 
-nr = len(pd.unique(dfts[0].Given))
-nc = len(pd.unique(dfts[0].ES))
 R = R2/R1
 b = a2max/a1max
 ess = np.sort(pd.unique(dfts[0].ES))
 rhos = 1.0 - 1.0/ess
 givens = np.sort(pd.unique(dfts[0].Given))[::-1]
+nc = len(rhos)
+nr = len(givens)
 givens[0] = 0.9999999
-Xrhos, Ygivens = np.meshgrid(rhos, givens)
-xeq = (fitness(0.0, 0.0) - fitness(0.5, 0.0))/(fitness(0.5, 0.5) - fitness(0.5, 0.0) - fitness(0.0, 0.5) + fitness(0.0, 0.0))
-xeq[Ygivens == 0.0] = 1.0
-xeq[xeq < 0.0] = 0.0
-xeq[xeq > 1.0] = 1.0
-Aeq = xeq*0.5
-helpeq = Aeq*R2*Ygivens 
-Feq = fitness(0.5, 0.5)*xeq*xeq + (fitness(0.5, 0.0) + fitness(0.0, 0.5))*xeq*(1.0 - xeq) + fitness(0.0, 0.0)*(1.0 - xeq)*(1.0 - xeq)
-Zs = [Aeq, helpeq, Feq, np.ones([nr, nc])*0.06, np.ones([nr, nc])*0.06]
+RR, GG = np.meshgrid(rhos, givens)
+a2Ds = np.full([nc, nr], 0.0)
+a2Cs = np.full([nc, nr], a2max/2.0)
+Ts = fitness(a2Cs, a2Ds)
+Rs = fitness(a2Cs, a2Cs)
+Ps = fitness(a2Ds, a2Ds)
+Ss = fitness(a2Ds, a2Cs)
+xeqss = np.full([nc, nr], 0.0)
+mask = (Ts < Rs) & (Ps < Ss)
+xeqss[mask] = 1.0
+mask = (Ts > Rs) & (Ps < Ss) & (Rs - Ss - Ts + Ps != 0.0)
+xeqss[mask] = (Ps[mask] - Ss[mask])/(Rs[mask] - Ss[mask] - Ts[mask] + Ps[mask])
+weqss = (Ts + Ss)*xeqss*(1.0 - xeqss) + Rs*xeqss*xeqss + Ps*(1.0 - xeqss)*(1.0 - xeqss)
+a2eqss = xeqss*a2max/2.0
+helpeqss = a2eqss*R2*GG 
+Zs = [a2eqss, helpeqss, weqss, np.ones([nc, nr])*0.06, np.ones([nc, nr])*0.06]
 
 fslabel=36 # Label font size
 fstick=24 # Tick font size

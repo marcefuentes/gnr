bd991213e60c6ba697bf76ebd20f7988c5d55835
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-07 21:15:56 +0100

    Update

--- bin/games.py
+++ bin/games.py
@@ -9,7 +9,7 @@ import time
 
 start_time = time.perf_counter ()
 
-alpha = 0.750
+alpha = 0.50
 R1 = 2.0
 R2 = 2.0
 a1max = 1.0
@@ -61,10 +61,6 @@ fstick = 18 # Tick font size
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
-yellow = [1.0, 0.7, 0.1, 1.0]
-red = [1.0, 0.2, 0.2, 1.0]
-blue = [0.2, 0.0, 0.4, 1.0]
-
 fig = plt.figure(figsize=(12, 12)) 
 fig.supylabel("Partner's share of $\it{A}$", x=0.04, y=0.520, fontsize=fslabel)
 fig.supxlabel("Substitutability of $\it{A}$", x=0.525, y=0.05, fontsize=fslabel)
@@ -78,8 +74,7 @@ axs = grid.subplots()
 
 a2 = np.linspace(0.0, limmatrix, num=npoints)
 X, Y = np.meshgrid(a2, a2)
-mycolors = ['cyan', blue, red, yellow]
-mycmap = ListedColormap(mycolors)
+Z = np.full([npoints, npoints], 0.0)
 
 for row, given in zip(axs, givens):
     for ax, rho in zip(row, rhos):
@@ -94,16 +89,13 @@ for row, given in zip(axs, givens):
         H = T[mask]
         T[mask] = S[mask]
         S[mask] = H
-        PD = ((T > R) & (P > S)).astype(int) 
-        SD = ((T >= R) & (P <= S)).astype(int) 
-        ND = ((T < R) & (P < S)).astype(int) 
-        Z = PD*1 + SD*2 + ND*3 + 1
+        Z[(T > R) & (P > S)] = 0.1
+        Z[(T >= R) & (P <= S)] = 0.5
+        Z[(T < R) & (P < S)] = 0.9
         Z = np.tril(Z, k=-1)
         Z = np.ma.masked_where(Z == 0.0, Z)
-        Z = Z - 1
-        cmap = cm.get_cmap(mycmap).copy()
-        cmap.set_bad(color='white')
-        ax.imshow(Z, origin='lower', cmap=cmap, vmin=0, vmax=3)
+        #cmap.set_bad(color='white')
+        ax.imshow(Z, origin='lower', cmap='magma', vmin=0, vmax=1)
         ax.set(xticks=[], yticks=[], xlim=(-11, npoints + 7), ylim=(-7, npoints + 11))
 
 axs[0, 0].set_title('a', fontsize=fslabel, weight='bold')
@@ -172,25 +164,25 @@ for row0, row1, given in zip(axs0, axs1, givens):
         if (T < R) & (P < S):
             x = 1.0
             weq = R
-            rgb = yellow
+            rgb = 0.9
         elif (T >= R) & (P <= S) & (R - S - T + P != 0.0):
             x = (P - S)/(R - S - T + P)
             weq = (T + S)*x*(1.0 - x) + R*x*x + P*(1.0 - x)*(1.0 - x)
-            rgb = red
+            rgb = 0.5
         elif (T > R) & (P > S):
             x = 0.0
             weq = P
-            rgb = blue
+            rgb = 0.1
         else:
             x = 0.0
             weq = P
-            rgb = 'cyan'
+            rgb = 0.0
         for a in a2:
             w.append(fitness(a2[0], a, given, rho)*(1.0 - x) + fitness(a2[1], a, given, rho)*x)   
         yaxis = [T, R, P, S]
-        ax0.plot(xaxis, yaxis, color=rgb, marker='o', markerfacecolor='white', linewidth=3, markersize=3)
+        ax0.plot(xaxis, yaxis, c=cm.magma(rgb), marker='o', markerfacecolor='white', linewidth=3, markersize=3)
         ax0.set(xticks=[], yticks=[], xlim=(0, 5), ylim=(0.0, 2.0))
-        ax1.plot(a2, w, linewidth=3, c=cm.magma(weq/vmax))
+        ax1.plot(a2, w, c=cm.magma(weq/vmax), linewidth=3)
         ax1.set(xticks=[], yticks=[], xlim=(0.0, a2max), ylim=(0.0, 2.0))
 
 axs0[0, 0].set_title('b', fontsize=fslabel, weight='bold')

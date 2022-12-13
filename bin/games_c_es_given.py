b7289b52f347ae109fb8dc922b7e754fdd524965
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-13 10:16:28 +0100

    Update

---
+++ bin/games_c_es_given.py
@@ -0,0 +1,174 @@
+#! /usr/bin/env python
+
+from glob import glob
+import os
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+import numpy as np
+import time
+
+start_time = time.perf_counter ()
+
+minalpha = 0.4
+maxalpha = 0.6
+minlog_es = -5.0
+maxlog_es = 5.0
+mingiven = 0.0
+maxgiven = 1.0
+
+num = 21    # Number of subplot rows and columns
+npoints = 128
+filename = 'games_c_rho_given'
+R1 = 2.0
+R2 = 2.0
+a1max = 1.0
+a2max = 1.0
+
+# Figure
+
+traitlabels = ['Effort to get $\it{A}$', 'Help', 'Fitness']
+traitvmaxs = [1.0, 2.0, 1.16]
+fslabel = 32 # Label font size
+fstick = 18 # Tick font size
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+letters = [['a', 'b', 'c'],
+            ['b', 'c', 'd'],
+            ['e', 'f', 'g']]
+
+def fitness(x, y, given, alpha, rho):
+    if isinstance(x, float): x = np.array([x])
+    if isinstance(y, float): y = np.array([y])
+    if isinstance(given, float): given = np.array([given])
+    if isinstance(alpha, float): alpha = np.array([alpha])
+    if isinstance(rho, float): rho = np.array([rho])
+    q1 = (a2max - y)*R1/b
+    q2 = y*R2*(1.0 - given) + x*R2*given
+    w = q1*q2
+    mask = (w > 0.0) & (rho == 0.0)
+    w[mask] = pow(q1[mask], alpha[mask])*pow(q2[mask], 1.0 - alpha[mask])
+    mask = (w > 0.0) & (rho < 0.0)
+    w[mask] = alpha[mask]*pow(q1[mask], rho[mask]) + (1.0 - alpha[mask])*pow(q2[mask], rho[mask])
+    mask = (w > 0.0) & (rho < 0.0)
+    w[mask] = pow(w[mask], 1.0/rho[mask])
+    mask = (rho > 0.0)
+    w[mask] = pow(alpha[mask]*pow(q1[mask], rho[mask]) + (1.0 - alpha[mask])*pow(q2[mask], rho[mask]), 1.0/rho[mask])
+    return w
+
+if mingiven != maxgiven:
+    movie = True
+    alphas = np.linspace(maxalpha, minalpha, num=num)
+    frames = []
+else:
+    movie = False 
+    alphas = np.array([minalpha])
+
+givens = np.linspace(maxgiven, mingiven, num=num)
+givens[0] = 0.999999
+log_ess = np.linspace(minlog_es, maxlog_es, num=num)
+rhos = 1.0 - 1.0/pow(2, log_ess)
+b = a2max/a1max
+Rq = R2/R1
+a2x = np.linspace(0.0, a2max, num=npoints)
+a2y = np.linspace(a2max, 0.0, num=npoints)
+X, Y = np.meshgrid(a2x, a2y)
+Z = np.zeros([npoints, npoints])
+RR, GG = np.meshgrid(rhos, givens)
+RR, G0 = np.meshgrid(rhos, np.zeros([num]))
+TT = b*Rq*(1.0 - GG)
+T0 = b*Rq*(1.0 - G0)
+
+minx = round(log_ess[0])
+maxx = round(log_ess[-1])
+miny = minigiven
+maxy = maxgiven
+
+xticklabels = [minx, round((minx + maxx)/2), maxx]
+yticklabels = [miny, (miny + maxy)/2, maxy]
+extent = 0, num, 0, num
+extentZ = 0, npoints*num, 0, npoints*num
+
+for alpha in alphas:
+
+    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(18, 18))
+    fig.delaxes(axs[0, 1])
+    fig.delaxes(axs[0, 2])
+    fig.supylabel("Partner's share of $\it{A}$", x=0.04, y=0.520, fontsize=fslabel)
+    fig.supxlabel("Substitutability of $\it{A}$", x=0.525, y=0.05, fontsize=fslabel)
+
+    if movie:
+        fig.text(0.80, 0.80, f'alpha\n{round(alpha,2)}', fontsize=fstick+4, color='grey', ha='right')
+
+    Q0 = Rq*pow(T0*(1.0 - alpha)/alpha, 1.0/(RR - 1.0))
+    a20ss = a2max/(1.0 + Q0*b)
+    Q = Rq*pow(TT*(1.0 - alpha)/alpha, 1.0/(RR - 1.0))
+    a2eqss = a2max/(1.0 + Q*b)
+
+    A = np.full([npoints, npoints], alpha)
+    Zss = np.empty((0, npoints*num))
+    for given in givens:
+        G = np.full([npoints, npoints], given)
+        Zs = np.empty((npoints, 0))
+        for rho in rhos:
+            Rh = np.full([npoints, npoints], rho)
+            T = fitness(Y, X, G, A, Rh)
+            R = fitness(Y, Y, G, A, Rh)
+            P = fitness(X, X, G, A, Rh)
+            S = fitness(X, Y, G, A, Rh)
+            mask = (R < P)
+            H = R[mask]
+            R[mask] = P[mask]
+            P[mask] = H
+            H = T[mask]
+            T[mask] = S[mask]
+            S[mask] = H
+            Z[(T > R) & (P > S)] = 0.1
+            Z[(T >= R) & (P <= S)] = 0.5
+            Z[(T < R) & (P < S)] = 0.9
+            Z[(T == R) & (P == R)] = 1.0
+            #Z = np.tril(Z, k=-1)
+            Z = np.ma.masked_where(Z == 0.0, Z)
+            Zs = np.append(Zs, Z, axis=1)
+        Zss = np.append(Zss, Zs, axis=0)
+
+    AA = np.full([num, num], alpha)
+    Mss = [[a20ss, a20ss*R2*GG, fitness(a20ss, a20ss, GG, AA, RR)], [a2eqss, a2eqss*R2*GG, fitness(a2eqss, a2eqss, GG, AA, RR)]]
+
+    for axrow, letterrow in zip(axs, letters):
+        for ax, letter, traitlabel in zip(axrow, letterrow, traitlabels):
+            if ax.get_subplotspec().is_first_row():
+                ax.set(xticks=[0, npoints*num/2, npoints*num], yticks=[0, npoints*num/2, npoints*num], xticklabels=[], yticklabels=yticklabels)
+                #ax.set_title(traitlabel, pad=50.0, fontsize=fslabel)
+                ax.text(0, npoints*num*1.035, letter, fontsize=fslabel, weight='bold')
+            else:
+                ax.set(xticks=[0, num/2, num], yticks=[0, num/2, num], xticklabels=[], yticklabels=[])
+                ax.text(0, num*1.035, letter, fontsize=fslabel, weight='bold')
+            if ax.get_subplotspec().is_last_row():
+                ax.set_xticklabels(xticklabels, fontsize=fstick)
+            if ax.get_subplotspec().is_first_col():
+                ax.set_yticklabels(yticklabels, fontsize=fstick) 
+
+    axs[0, 0].imshow(Zss, extent=extentZ, cmap='magma', vmin=0, vmax=1)
+
+    for row, Ms in zip(axs[1:], Mss):
+        for ax, M, traitvmax in zip(row, Ms, traitvmaxs):
+            ax.imshow(M, extent=extent, cmap='magma', vmin=0, vmax=traitvmax)
+
+    if movie:
+        plt.savefig('temp.png', transparent=False)
+        frames.append(iio.imread('temp.png'))
+        os.remove('temp.png')
+    else:
+        plt.savefig(filename + '.png', transparent=False)
+
+    plt.close()
+
+if movie:
+    iio.mimsave(filename + '.gif', frames)
+
+end_time = time.perf_counter ()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')

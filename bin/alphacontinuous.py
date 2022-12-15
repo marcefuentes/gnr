5b0383fa5e2c566d1a9c90729d48c566b98924e8
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-15 13:39:04 +0100

    Update

---
+++ bin/alphacontinuous.py
@@ -0,0 +1,177 @@
+#! /usr/bin/env python
+
+from glob import glob
+from math import log
+import os
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+
+start_time = time.perf_counter ()
+
+traits = ['ChooseGrainmean', 'MimicGrainmean']
+traitlabels = ['Game types', 'Sensitivity for\nchoosing partner', 'Sensitivity for\nmimicking partner']
+folders = ['p', 'r']
+alphafolders = ['alpha25', 'alpha50', 'alpha75']
+alphas = [0.25, 0.50, 0.75]
+
+movie = False
+if movie:
+    frames = []
+
+npoints = 128
+filename = 'alphacontinuous'
+R1 = 2.0
+R2 = 2.0
+a1max = 1.0
+a2max = 1.0
+
+# Figure
+
+fslabel=24 # Label font size
+fstick=16 # Tick font size
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+letters = [['a', 'b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i']]
+
+def fitness(x, y, given, alpha, rho):
+    q1 = (a2max - y)*R1/b
+    q2 = y*R2*(1.0 - given) + x*R2*given
+    w = q1*q2
+    mask = (w > 0.0) & (rho == 0.0)
+    w[mask] = pow(q1[mask], 1.0 - alpha[mask])*pow(q2[mask], alpha[mask])
+    mask = (w > 0.0) & (rho < 0.0)
+    w[mask] = (1.0 - alpha[mask])*pow(q1[mask], rho[mask]) + alpha[mask]*pow(q2[mask], rho[mask])
+    mask = (w > 0.0) & (rho < 0.0)
+    w[mask] = pow(w[mask], 1.0/rho[mask])
+    mask = (rho > 0.0)
+    w[mask] = pow((1.0 - alpha[mask])*pow(q1[mask], rho[mask]) + alpha[mask]*pow(q2[mask], rho[mask]), 1.0/rho[mask])
+    return w
+
+# Simulations
+
+dfss = []
+for alphafolder in alphafolders:
+    dfs = []
+    for folder in folders:
+        df = pd.concat(map(pd.read_csv, glob(os.path.join(alphafolder, 'continuous', folder, '*.csv'))), ignore_index=True)
+        dfs.append(df)
+    dfss.append(dfs)
+
+df = dfss[0][0]
+ts = df.Time.unique()
+if movie == False: ts = [ts[-1]]
+givens = np.sort(pd.unique(df.Given))[::-1]
+ess = np.sort(pd.unique(df.ES))
+givens[0] = 0.9999999
+rhos = 1.0 - 1.0/ess
+nr = len(givens)
+nc = len(rhos)
+mingiven = givens[-1]
+maxgiven = givens[0]
+
+# Theory
+
+b = a2max/a1max
+Rq = R2/R1
+a2x = np.linspace(0.0, a2max, num=npoints)
+a2y = np.linspace(a2max, 0.0, num=npoints)
+X, Y = np.meshgrid(a2x, a2y)
+RR, GG = np.meshgrid(rhos, givens)
+TT = b*Rq*(1.0 - GG)
+
+minx = round(log_ess[0])
+maxx = round(log_ess[-1])
+miny = mingiven
+maxy = maxgiven
+
+xticklabels = [minx, round((minx + maxx)/2), maxx]
+yticklabels = [miny, (miny + maxy)/2, maxy]
+extent = 0, num, 0, num
+extentZ = 0, npoints*num, 0, npoints*num
+
+Zsss = []
+for alpha in alphas:
+    QQ = Rq*pow(TT*alpha/(1.0 - alpha), 1.0/(RR - 1.0))
+    Z = np.zeros([npoints, npoints])
+
+    A = np.full([npoints, npoints], alpha)
+    Zss = np.empty((0, npoints*num))
+    for given in givens:
+        G = np.full([npoints, npoints], given)
+        Zs = np.empty((npoints, 0))
+        for rho in rhos:
+            Z = np.zeros([npoints, npoints])
+            Rh = np.full([npoints, npoints], rho)
+            T = fitness(Y, X, G, A, Rh)
+            R = fitness(Y, Y, G, A, Rh)
+            P = fitness(X, X, G, A, Rh)
+            S = fitness(X, Y, G, A, Rh)
+            mask = (R < P)
+            H = R[mask]
+            R[mask] = P[mask]
+            P[mask] = H
+            H = T[mask]
+            T[mask] = S[mask]
+            S[mask] = H
+            Z[(T > R) & (P > S)] = 0.1
+            Z[(T >= R) & (P <= S)] = 0.5
+            Z[(T < R) & (P < S)] = 0.9
+            Z[(T == R) & (P == R)] = 1.0
+            #Z = np.tril(Z, k=-1)
+            Z = np.ma.masked_where(Z == 0.0, Z)
+            Zs = np.append(Zs, Z, axis=1)
+        Zss = np.append(Zss, Zs, axis=0)
+    Zsss.append(Zss)
+
+fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1)))
+fig.supxlabel('Substitutability of $\it{A}$', x=0.513, y=0.05, fontsize=fslabel*1.25)
+fig.supylabel('Partner\'s share of $\it{A}$', x=0.05, y=0.493, fontsize=fslabel*1.25, ha='center')
+
+# Plots
+
+minx = round(log(ess[0], 2))
+maxx = round(log(ess[-1], 2))
+xticklabels = [minx, round((minx + maxx)/2), maxx]
+yticklabels = [0.0, 0.5, 1.0]
+extent = 0, nr, 0, nc
+
+for axrow, letterrow in zip(axs, letters):
+    for ax, letter, traitlabel in zip(axrow, letterrow, traitlabels):
+        ax.text(0, nr*1.035, letter, fontsize=fslabel, weight='bold')
+        ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
+        if ax.get_subplotspec().is_first_row():
+            ax.set_title(traitlabel, pad=50.0, fontsize=fslabel)
+        if ax.get_subplotspec().is_last_row():
+            ax.set_xticklabels(xticklabels, fontsize=fstick)
+        if ax.get_subplotspec().is_first_col():
+            ax.set_yticklabels(yticklabels, fontsize=fstick) 
+
+for axrow, Zss in zip(axs, Zsss):
+    axrow[0].imshow(Zss, extent=extent, cmap='magma', vmin=0, vmax=1.0)
+for t in ts:
+    for axrow, dfs in zip(axs, dfss):
+        for ax, df, trait in zip(axrow[1:], dfs, traits):
+            df = df.loc[df.Time == t].copy()
+            df[trait] = 1.0 - df[trait]
+            df_piv = pd.pivot_table(df, values=trait, index=['Given'], columns=['ES']).sort_index(axis=0, ascending=False)
+            ax.imshow(df_piv, extent=extent, cmap='magma', vmin=0, vmax=1.0)
+    if movie:
+        plt.savefig('temp.png', transparent=False)
+        frames.append(iio.imread('temp.png'))
+        os.remove('temp.png')
+    else:
+        plt.savefig(filename + '.png', transparent=False)
+
+plt.close()
+
+if movie:
+    iio.mimsave(filename + '.gif', frames)
+
+end_time = time.perf_counter ()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')
---
+++ bin/games_d_es_given.py
@@ -0,0 +1,246 @@
+#! /usr/bin/env python
+
+from glob import glob
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+import os
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import time
+
+start_time = time.perf_counter ()
+
+minalpha = 0.4
+maxalpha = 0.6
+minlog_es = -5.0
+maxlog_es = 5.0
+mingiven = 0.0
+maxgiven = 1.0
+
+num = 21    # Number of subplot rows and columns
+filename = 'games_d_es_given'
+R1 = 2.0
+R2 = 2.0
+a1max = 1.0
+a2max = 1.0
+
+# Figure
+
+traitlabels = ['Effort to get $\it{A}$', 'Help', 'Fitness']
+traitvmaxs = [1.0, 2.0, 1.16]
+fslabel = 32 # Label font size
+fstick = 18 # Tick font size
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+letters = [['a', 'b', 'c'],
+            ['b', 'c', 'd'],
+            ['e', 'f', 'g']]
+
+def fitness(x, y):
+    q1 = (a2max - y)*R1/b
+    q2 = y*R2*(1.0 - given) + x*R2*given
+    w = q1*q2
+    mask = (w > 0.0) & (RR == 0.0)
+    w[mask] = pow(q1[mask], 1.0 - AA[mask])*pow(q2[mask], AA[mask])
+    mask = (w > 0.0) & (RR < 0.0)
+    w[mask] = (1.0 - AA[mask])*pow(q1[mask], RR[mask]) + AA[mask]*pow(q2[mask], RR[mask])
+    mask = (w > 0.0) & (RR < 0.0)
+    w[mask] = pow(w[mask], 1.0/RR[mask])
+    mask = (RR > 0.0)
+    w[mask] = pow((1.0 - AA[mask])*pow(q1[mask], RR[mask]) + AA[mask]*pow(q2[mask], RR[mask]), 1.0/RR[mask])
+    return w
+
+if minalpha != maxalpha:
+    movie = True
+    alphas = np.linspace(maxalpha, minalpha, num=num)
+    frames = []
+else:
+    movie = False 
+    alphas = np.array([minalpha])
+
+givens = np.linspace(maxgiven, mingiven, num=num)
+givens[0] = 0.999999 #?
+log_ess = np.linspace(minlog_es, maxlog_es, num=num)
+rhos = 1.0 - 1.0/pow(2, log_ess)
+b = a2max/a1max
+RR, AA = np.meshgrid(rhos, alphas)
+a20 = np.full([num, num], 0.0)
+a21 = np.full([num, num], a2max/2.0)
+a22 = np.full([num, num], a2max)
+given = 0.0
+w00 = fitness(a20, a20)
+w11 = fitness(a21, a21)
+w22 = fitness(a22, a22)
+a2optimal = a20
+mask = (w11 > w00)
+a2optimal[mask] = a21[mask]
+mask = (w22 > w11)
+a2optimal[mask] = a22[mask]
+woptimal = fitness(a2optimal, a2optimal)
+
+minx = round(log_ess[0])
+maxx = round(log_ess[-1])
+miny = mingiven
+maxy = maxgiven
+
+xticklabels = [minx, round((minx + maxx)/2), maxx]
+yticklabels = [miny, (miny + maxy)/2, maxy]
+extent = 0, num, 0, num
+black = np.full((num, num, 4), [0.2, 0.0, 0.2, 1.0])
+cyan = np.full((num, num, 4), [0.0, 1.0, 1.0, 1.0])
+white = np.full((num, num, 4), [1.0, 1.0, 1.0, 1.0])
+green = np.full((num, num, 4), [0.0, 1.0, 0.0, 1.0])
+
+for given in reversed(givens):
+
+    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(18, 18))
+    fig.delaxes(axs[0, 1])
+    fig.delaxes(axs[0, 2])
+    fig.supylabel("Value of $\it{B}$", x=0.04, y=0.520, fontsize=fslabel)
+    fig.supxlabel("Substitutability of $\it{A}$", x=0.525, y=0.05, fontsize=fslabel)
+
+    if movie:
+        fig.text(0.80, 0.80, f'given\n{given:4.2f}', fontsize=fstick+4, color='grey', ha='right')
+
+    Z = np.full([num, num, 4], [0.0, 1.0, 0.0, 1.0])
+    a20 = np.full([num, num], 0.0)
+    a21 = np.full([num, num], a2max/2.0)
+    a2 = np.full([num, num], 0.0)
+    w = np.full([num, num], 0.0)
+    x = np.full([num, num], 0.0)
+    w01 = fitness(a20, a21)
+    w10 = fitness(a21, a20)
+    w12 = fitness(a21, a22)
+    w21 = fitness(a22, a21)
+    w02 = fitness(a20, a22)
+    w20 = fitness(a22, a20)
+
+    mask0 = (woptimal == w00)
+    T = w01
+    R = w00
+    P = w11
+    S = w10
+    mask = (mask0 & (T < R) & (P < S))
+    Z[mask] = white[mask]
+    a2[mask] = a20[mask]
+    w[mask] = R[mask]
+    mask = (mask0 & (T >= R) & (P <= S))
+    Z[mask] = cyan[mask]
+    x[mask] = (P[mask] - S[mask])/(R[mask] - S[mask] - T[mask] + P[mask])
+    a2[mask] = a20[mask]*x[mask] + a21[mask]*(1.0 - x[mask])
+    w[mask] = (T[mask] + S[mask])*x[mask]*(1.0 - x[mask]) + R[mask]*x[mask]*x[mask] + P[mask]*(1.0 - x[mask])*(1.0 - x[mask])
+    mask = (mask0 & (T > R) & (P > S))
+    Z[mask] = black[mask]
+    a2[mask] = a21[mask]
+    w[mask] = P[mask]
+
+    mask0 = (woptimal == w11) & (w20 > w22)
+    T = w10
+    R = w11
+    P = w00
+    S = w01
+    mask = (mask0 & (T < R) & (P < S))
+    Z[mask] = white[mask]
+    a2[mask] = a21[mask]
+    w[mask] = R[mask]
+    mask = (mask0 & (T >= R) & (P <= S))
+    Z[mask] = cyan[mask]
+    x[mask] = (P[mask] - S[mask])/(R[mask] - S[mask] - T[mask] + P[mask])
+    a2[mask] = a21[mask]*x[mask] + a20[mask]*(1.0 - x[mask])
+    w[mask] = (T[mask] + S[mask])*x[mask]*(1.0 - x[mask]) + R[mask]*x[mask]*x[mask] + P[mask]*(1.0 - x[mask])*(1.0 - x[mask])
+    mask = (mask0 & (T > R) & (P > S))
+    Z[mask] = black[mask]
+    a2[mask] = a20[mask]
+    w[mask] = P[mask]
+
+    mask0 = (woptimal == w11) & (w20 <= w22)
+    T = w12
+    R = w11
+    P = w22
+    S = w21
+    mask = (mask0 & (T < R) & (P < S))
+    Z[mask] = white[mask]
+    a2[mask] = a21[mask]
+    w[mask] = R[mask]
+    mask = (mask0 & (T >= R) & (P <= S))
+    Z[mask] = cyan[mask]
+    x[mask] = (P[mask] - S[mask])/(R[mask] - S[mask] - T[mask] + P[mask])
+    a2[mask] = a21[mask]*x[mask] + a22[mask]*(1.0 - x[mask])
+    w[mask] = (T[mask] + S[mask])*x[mask]*(1.0 - x[mask]) + R[mask]*x[mask]*x[mask] + P[mask]*(1.0 - x[mask])*(1.0 - x[mask])
+    mask = (mask0 & (T > R) & (P > S))
+    Z[mask] = black[mask]
+    a2[mask] = a22[mask]
+    w[mask] = P[mask]
+
+    mask0 = (woptimal == w22) & (w10 < w11)
+    T = w21
+    R = w22
+    P = w11
+    S = w12
+    mask = (mask0 & (T < R) & (P < S))
+    Z[mask] = white[mask]
+    a2[mask] = a22[mask]
+    w[mask] = R[mask]
+    mask = (mask0 & (T >= R) & (P <= S))
+    Z[mask] = cyan[mask]
+    x[mask] = (P[mask] - S[mask])/(R[mask] - S[mask] - T[mask] + P[mask])
+    a2[mask] = a22[mask]*x[mask] + a21[mask]*(1.0 - x[mask])
+    w[mask] = (T[mask] + S[mask])*x[mask]*(1.0 - x[mask]) + R[mask]*x[mask]*x[mask] + P[mask]*(1.0 - x[mask])*(1.0 - x[mask])
+    mask = (mask0 & (T > R) & (P > S))
+    Z[mask] = black[mask]
+    a2[mask] = a21[mask]
+    w[mask] = P[mask]
+
+    mask0 = (woptimal == w22) & (w10 >= w11)
+    T = w10
+    R = w11
+    P = w00
+    S = w01
+    mask = (mask0 & (T < R) & (P < S))
+    Z[mask] = white[mask]
+    a2[mask] = a21[mask]
+    w[mask] = R[mask]
+    mask = (mask0 & (T >= R) & (P <= S))
+    Z[mask] = cyan[mask]
+    x[mask] = (P[mask] - S[mask])/(R[mask] - S[mask] - T[mask] + P[mask])
+    a2[mask] = a21[mask]*x[mask] + a20[mask]*(1.0 - x[mask])
+    w[mask] = (T[mask] + S[mask])*x[mask]*(1.0 - x[mask]) + R[mask]*x[mask]*x[mask] + P[mask]*(1.0 - x[mask])*(1.0 - x[mask])
+    mask = (mask0 & (T > R) & (P > S))
+    Z[mask] = black[mask]
+    a2[mask] = a20[mask]
+    w[mask] = P[mask]
+
+    Mss = [[a2optimal, a2optimal*R2*given, woptimal], [a2, a2*R2*given, w]]
+
+    for axrow, letterrow in zip(axs, letters):
+        for ax, letter, traitlabel in zip(axrow, letterrow, traitlabels):
+            ax.set(xticks=[0, num/2, num], yticks=[0, num/2, num], xticklabels=[], yticklabels=[])
+            ax.text(0, num*1.035, letter, fontsize=fslabel, weight='bold')
+            if ax.get_subplotspec().is_last_row():
+                ax.set_xticklabels(xticklabels, fontsize=fstick)
+            if ax.get_subplotspec().is_first_col():
+                ax.set_yticklabels(yticklabels, fontsize=fstick) 
+
+    axs[0, 0].imshow(Z, extent=extent)
+
+    for row, Ms in zip(axs[1:], Mss):
+        for ax, M, traitvmax in zip(row, Ms, traitvmaxs):
+            ax.imshow(M, extent=extent, cmap='magma', vmin=0, vmax=traitvmax)
+
+    if movie:
+        plt.savefig('temp.png', transparent=False)
+        frames.append(iio.imread('temp.png'))
+        os.remove('temp.png')
+    else:
+        plt.savefig(filename + '.png', transparent=False)
+
+    plt.close()
+
+if movie:
+    iio.mimsave(filename + '.gif', frames)
+
+end_time = time.perf_counter ()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')

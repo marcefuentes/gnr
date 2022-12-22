caca07d7b2a9e1d02aa45706b1467cd1876460a1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-22 09:36:02 +0100

    Update

--- bin/replacecolumn.py
+++ bin/replacecolumn.py
@@ -3,6 +3,14 @@
 import os
 import pandas as pd
 
-for dirs, files in os.walk():
-    for subdir in dirs:
-        print(subdir)
+alpha = 0.5
+
+for root, dirs, files in os.walk('.'):
+    for filename in files:
+        if '.csv' in filename:
+            df = pd.read_csv(os.path.join(root, filename))
+            newcol = [alpha]*len(df.index)
+            df['alpha'] = newcol
+            df.drop(['N'], inplace=True, axis=1)
+            df.insert(2, 'alpha', df.pop('alpha'))
+            df.to_csv(os.path.join(root, filename), index=False)

bb04e8e1da4c1f514351719c67564bdc2676c1fc
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-23 15:01:01 +0100

    Change given to Given

--- bin/replacecolumn.py
+++ bin/replacecolumn.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
+from math import log
 import os
 import pandas as pd
 
-oldcolname = 'N'
-newcolname = 'given'
-newcolvalue = 0.95
+oldcolname = 'ES'
+newcolname = 'logES'
+#newcolvalue = 0.95
 
 for root, dirs, files in os.walk('.'):
     for filename in files:
         if ('csv' in filename) or ('frq' in filename):
             df = pd.read_csv(os.path.join(root, filename))
-            newcolvalues = [newcolvalue]*len(df.index)
-            df[newcolname] = newcolvalues
+            #newcolvalues = [newcolvalue]*len(df.index)
+            #df[newcolname] = newcolvalues
+            df[newcolname] = log(df[oldcolname], 2.0)
             df.drop(oldcolname, axis=1, inplace=True)
             df.insert(2, newcolname, df.pop(newcolname))
             df.to_csv(os.path.join(root, filename), float_format='%.6f', index=False)

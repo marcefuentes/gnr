da276fe7751077f435c3883fdbef529cc74fba0b
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-25 11:54:12 +0100

    Update

--- bin/icurves_es_alpha.py
+++ bin/icurves_es_alpha.py
@@ -13,10 +13,10 @@ start_time = time.perf_counter ()
 
 minalpha = 0.1
 maxalpha = 0.9
-minlog_es = -5.0
-maxlog_es = 5.0
-mingiven = 0.0
-maxgiven = 1.0
+minloges = -5.0
+maxloges = 5.0
+mingiven = 0.95
+maxgiven = 0.95
 
 num = 5    # Number of subplot rows & columns
 npoints = 128
@@ -28,8 +28,6 @@ R1 = 2.0
 R2 = 2.0
 a1max = 1.0
 a2max = 1.0
-
-# Figure
 
 fslabel = 32 # Label font size
 fstick = 18 # Tick font size
@@ -71,16 +69,22 @@ else:
 
 Rq = R2/R1
 b = a2max/a1max
+nc = num
+nr = num
 a1_budget = np.linspace(0.0, a1max, num=3)
 q2_budget = (a2max - b*a1_budget)*R2
 q1_budget = a1_budget*R1
 q1_ic = np.linspace(0.0, a1max*R1, num=npoints)
-givens[-1] = 0.9999999
-alphas = np.linspace(maxalpha, minalpha, num=num)
-log_ess = np.linspace(minlog_es, maxlog_es, num=num)
-rhos = 1.0 - 1.0/pow(2, log_ess)
+if givens[-1] == 0.9999999:
+    givens[-1] = 0.9999999
+alphas = np.linspace(maxalpha, minalpha, num=nr)
+logess = np.linspace(minloges, maxloges, num=nc)
+rhos = 1.0 - 1.0/pow(2, logess)
 RR, AA = np.meshgrid(rhos, alphas)
 wis = np.linspace(2.0/(n_ic + 1), 2.0*n_ic/(n_ic + 1), num=n_ic)
+
+xlabel = 'Substitutability of $\it{B}$'
+ylabel = 'Value of $\it{B}$'
 
 icsss = []
 for alpha in alphas:
@@ -92,18 +96,24 @@ for alpha in alphas:
         icss.append(ics)
     icsss.append(icss)
 
+fig = plt.figure(figsize=(8, 8))
+fig.supxlabel(xlabel, x=0.555, y=0.03, fontsize=fslabel)
+fig.supylabel(ylabel, x=0.05, y=0.52, fontsize=fslabel)
+grid = fig.add_gridspec(nrows=nr, ncols=nc, left=0.22, right=0.9, top=0.86, bottom=0.176, wspace=0, hspace=0)
+axs = grid.subplots()
+
+for row in axs:
+    for ax in row:
+        ax.set(xticks=[], yticks=[], xlim=(0.0, a1max*R1), ylim=(0.0, a2max*R2))
+for ax, loges in zip(axs[-1, ::every], logess[::every]):
+    ax.set_xlabel(round(loges), fontsize=fstick)
+for ax, alpha in zip(axs[::every, 0], alphas[::every]):
+    ax.set_ylabel(f'{alpha:1.1f}', rotation='horizontal', horizontalalignment='right', verticalalignment='center', fontsize=fstick)
+
 for given in givens:
 
-    fig = plt.figure(figsize=(8, 8)) 
-    fig.supylabel("Value of $\it{B}$", x=0.05, y=0.52, fontsize=fslabel)
-    fig.supxlabel("Substitutability of $\it{B}$", x=0.555, fontsize=fslabel)
-
     if movie:
-        fig.text(0.80, 0.90, f'given: {given:4.2f}', fontsize=fstick+4, color='grey', ha='right')
-
-    grid = fig.add_gridspec(nrows=num, ncols=num, left=0.22, right=0.9, top=0.86, bottom=0.176, wspace=0, hspace=0)
-
-    axs = grid.subplots()
+        text = fig.text(0.80, 0.90, f'given: {given:4.2f}', fontsize=fstick, color='grey', ha='right')
 
     T = b*Rq*(1.0 - given)
     Q = Rq*pow(T*AA/(1.0 - AA), 1.0/(RR - 1.0))
@@ -114,25 +124,23 @@ for given in givens:
     for row, alpha, q2s, ws, icss in zip(axs, alphas, q2ss, wss, icsss):
         budget0 = q2_budget*(1.0 - given)
         for ax, rho, ics, q2eq, weq in zip(row, rhos, icss, q2s, ws):
+            for line in ax.get_lines():
+                line.remove()
             for ic in ics:
                 ax.plot(q1_ic, ic, c='0.850')
             budget = budget0 + q2eq*given
             ax.plot(q1_budget, budget, c='green', alpha=0.8)
             ax.plot(q1_ic, indifference(q1_ic, weq, alpha, rho), linewidth=4, alpha= 0.8, c=cm.magma(weq/1.8))
-            ax.set(xticks=[], yticks=[], xlim=(0.0, a1max*R1), ylim=(0.0, a2max*R2))
-    for ax, log_es in zip(axs[-1, ::every], log_ess[::every]):
-        ax.set_xlabel(round(log_es), fontsize=fstick)
-    for ax, alpha in zip(axs[::every, 0], alphas[::every]):
-        ax.set_ylabel(f'{alpha:1.2f}', rotation='horizontal', horizontalalignment='right', verticalalignment='center', fontsize=fstick)
 
     if movie:
         plt.savefig('temp.png', transparent=False)
         frames.append(iio.imread('temp.png'))
         os.remove('temp.png')
+        text.remove()
     else:
         plt.savefig(filename + '.png', transparent=False)
 
-    plt.close()
+plt.close()
 
 if movie:
     iio.mimsave(filename + '.gif', frames)

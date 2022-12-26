aafc9149629ba30c78991c65346b1f2fef6cb149
Marcelino Fuentes <marcelinofuentes@gmail.com>
2022-12-26 10:55:59 +0100

    Update

--- bin/replacecolumn.py
+++ bin/replacecolumn.py
@@ -11,10 +11,12 @@ newcolname = 'logES'
 for root, dirs, files in os.walk('.'):
     for filename in files:
         if ('csv' in filename) or ('frq' in filename):
+            #print(filename)
             df = pd.read_csv(os.path.join(root, filename))
             #newcolvalues = [newcolvalue]*len(df.index)
             #df[newcolname] = newcolvalues
-            df[newcolname] = np.log2(df[oldcolname])
-            df.drop(oldcolname, axis=1, inplace=True)
-            df.insert(2, newcolname, df.pop(newcolname))
-            df.to_csv(os.path.join(root, filename), float_format='%.6f', index=False)
+            if oldcolname in df:
+                df[newcolname] = np.log2(df[oldcolname])
+                df.drop(oldcolname, axis=1, inplace=True)
+                df.insert(2, newcolname, df.pop(newcolname))
+                df.to_csv(os.path.join(root, filename), float_format='%.6f', index=False)

30f909693a7bcf007a52261bdfd2ce28e7790b37
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-01-02 15:20:01 +0100

    Update

--- bin/gamesd.py
+++ bin/gamesd.py
@@ -13,12 +13,13 @@ alphamin = 0.1
 alphamax = 0.9
 logesmin = -5.0
 logesmax = 5.0
-givenmin = 0.95
-givenmax = 0.95
+givenmin = 0.0
+givenmax = 1.0
 
 num = 21    # Number of subplot rows and columns
+numa2 = 2
 ngiven = 21
-filename = 'gamesd'
+filename = 'gamesd3'
 R1 = 2.0
 R2 = 2.0
 a1max = 1.0
@@ -43,7 +44,7 @@ def fitness(x, y, given, alpha, rho):
     w[mask] = pow((1.0 - alpha[mask])*pow(q1[mask], rho[mask]) + alpha[mask]*pow(q2[mask], rho[mask]), 1.0/rho[mask])
     return w
 
-def gametypes(a2c, a2d):
+def gametypes(a2c, a2d, Z):
     mask = (mask0 & (T < R) & (P < S))
     Z[mask] = nodilemma
     a2eq[mask] = a2c[mask]
@@ -86,6 +87,7 @@ traitvmaxs = [a2max, fitness(np.array([a2max]), np.array([a2max]), np.array([0.0
 xticklabels = [round(xmin), round((xmin + xmax)/2), round(xmax)]
 yticklabels = [round(ymin, 1), round((ymin + ymax)/2, 1), round(ymax, 1)]
 extent = 0, nc, 0, nr
+extenta2 = 0, nc, 0, nr*numa2
 prisoner = [0.5, 0.0, 0.0, 1.0]
 snowdrift = [0.0, 1.0, 1.0, 1.0]
 nodilemma = [1.0, 1.0, 1.0, 1.0]
@@ -113,20 +115,21 @@ fig.supylabel(ylabel, x=0.04, y=0.510, fontsize=fslabel)
 letter = ord('b')
 for axrow in axs:
     for ax in axrow:
-        ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
         if ax.get_subplotspec().is_first_row():
+            ax.set(xticks=[0, nc/2, nc], yticks=[0, numa2*nr/2, numa2*nr], xticklabels=[], yticklabels=[])
             ax.set_title('Game types', pad=50.0, fontsize=fslabel)
-            ax.text(0, nr*1.035, 'a', fontsize=fslabel, weight='bold')
+            ax.text(0, numa2*nr*1.035, 'a', fontsize=fslabel, weight='bold')
             pos = ax.get_position()
             newpos = [pos.x0, pos.y0+0.04, pos.width, pos.height]
             ax.set_position(newpos)
         else:
+            ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
             ax.text(0, nr*1.035, chr(letter), fontsize=fslabel, weight='bold')
             letter += 1
-        if ax.get_subplotspec().is_last_row():
-            ax.set_xticklabels(xticklabels, fontsize=fstick)
         if ax.get_subplotspec().is_first_col():
             ax.set_yticklabels(yticklabels, fontsize=fstick) 
+        if ax.get_subplotspec().is_last_row():
+            ax.set_xticklabels(xticklabels, fontsize=fstick)
 for ax, traitlabel in zip(axs[1], traitlabels):
     ax.set_title(traitlabel, pad=50.0, fontsize=fslabel)
 
@@ -135,7 +138,8 @@ for given in givens:
     if movie:
         text = fig.text(0.90, 0.90, f'given: {given:4.2f}', fontsize=fstick, color='grey', ha='right')
 
-    Z = np.full([nr, nc, 4], green)
+    Z0 = np.full([nr, nc, 4], green)
+    Z1 = np.full([nr, nc, 4], green)
     a2eq = np.copy(zeros)
     weq = np.copy(zeros)
     xeq = np.copy(zeros)
@@ -146,44 +150,41 @@ for given in givens:
     w02 = fitness(a20, a22, given, AA, RR)
     w20 = fitness(a22, a20, given, AA, RR)
 
-    mask0 = (wsocial == w00)
+    mask0 = (w00 > w11)
     T = np.copy(w01)
     R = np.copy(w00)
     P = np.copy(w11)
     S = np.copy(w10)
-    gametypes(a20, a21)
+    gametypes(a20, a21, Z0)
 
-    mask0 = (wsocial == w11) & (w20 > w22)
+    mask0 = (w00 < w11)
     T = np.copy(w10)
     R = np.copy(w11)
     P = np.copy(w00)
     S = np.copy(w01)
-    gametypes(a21, a20)
+    gametypes(a21, a20, Z0)
 
-    mask0 = (wsocial == w11) & (w20 <= w22)
+    mask0 = (w11 > w22)
     T = np.copy(w12)
     R = np.copy(w11)
     P = np.copy(w22)
     S = np.copy(w21)
-    gametypes(a21, a22)
+    gametypes(a21, a22, Z1)
 
-    mask0 = (wsocial == w22) & (w10 < w11)
+    mask0 = (w11 < w22)
     T = np.copy(w21)
     R = np.copy(w22)
     P = np.copy(w11)
     S = np.copy(w12)
-    gametypes(a22, a21)
-
-    mask0 = (wsocial == w22) & (w10 >= w11)
-    T = np.copy(w10)
-    R = np.copy(w11)
-    P = np.copy(w00)
-    S = np.copy(w01)
-    gametypes(a21, a20)
+    gametypes(a22, a21, Z1)
 
     Mss = [[a2social, wsocial], [a2eq, weq]]
 
-    axs[0, 0].imshow(Z, extent=extent)
+    Z = np.full([nr*numa2, nc, 4], green)
+    Z[::2,:] = Z1
+    Z[1::2,:] = Z0
+
+    axs[0, 0].imshow(Z, extent=extenta2, aspect=1.0/numa2)
 
     for row, Ms in zip(axs[1:], Mss):
         for ax, M, traitvmax in zip(row, Ms, traitvmaxs):

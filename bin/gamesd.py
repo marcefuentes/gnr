674e55f15dcab6809cd2144f317db90d3048da8e
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-01-23 17:28:02 +0100

    Update

--- bin/gamesd.py
+++ bin/gamesd.py
@@ -14,11 +14,12 @@ alphamin = 0.1
 alphamax = 0.9
 logesmin = -5.0
 logesmax = 5.0
-givenmin = 0.0
-givenmax = 1.0
+givenmin = 0.95
+givenmax = 0.95
+a2low = 0.1
+a2high = 0.6
 
 num = 21    # Number of subplot rows and columns
-numa2 = 2
 ngiven = 21
 filename = 'gamesd'
 
@@ -29,8 +30,7 @@ plt.rcParams['ps.fonttype'] = 42
 
 if givenmin != givenmax:
     movie = True
-    givens0 = np.linspace(givenmin, givenmax, num=ngiven)
-    givens = np.concatenate((givens0, np.flip(givens0)))
+    givens = np.linspace(givenmin, givenmax, num=ngiven)
     frames = []
 else:
     movie = False 
@@ -54,7 +54,7 @@ traitvmaxs = [mymodule.a2max, mymodule.fitness(np.array([mymodule.a2max]), np.ar
 xticklabels = [round(xmin), round((xmin + xmax)/2), round(xmax)]
 yticklabels = [round(ymin, 1), round((ymin + ymax)/2, 1), round(ymax, 1)]
 extent = 0, nc, 0, nr
-extenta2 = 0, nc, 0, nr*numa2
+extenta2 = 0, nc, 0, nr
 
 zeros = np.zeros([nr, nc])
 a20 = np.copy(zeros)
@@ -71,24 +71,20 @@ a2social[mask] = a22[mask]
 wsocial = mymodule.fitness(a2social, a2social, zeros, AA, RR)
 
 fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(12, 18))
-fig.delaxes(axs[0, 1])
 fig.supxlabel(xlabel, x=0.515, y=0.03, fontsize=fslabel)
 fig.supylabel(ylabel, x=0.04, y=0.510, fontsize=fslabel)
 
-letter = ord('b')
+letter = ord('a')
 for axrow in axs:
     for ax in axrow:
+        ax.text(0, nr*1.035, chr(letter), fontsize=fslabel, weight='bold')
+        letter += 1
+        ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
         if ax.get_subplotspec().is_first_row():
-            ax.set(xticks=[0, nc/2, nc], yticks=[0, nr*numa2/2, nr*numa2], xticklabels=[], yticklabels=[])
             ax.set_title('Game types', pad=50.0, fontsize=fslabel)
-            ax.text(0, nr*numa2*1.035, 'a', fontsize=fslabel, weight='bold')
             pos = ax.get_position()
             newpos = [pos.x0, pos.y0+0.04, pos.width, pos.height]
             ax.set_position(newpos)
-        else:
-            ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
-            ax.text(0, nr*1.035, chr(letter), fontsize=fslabel, weight='bold')
-            letter += 1
         if ax.get_subplotspec().is_first_col():
             ax.set_yticklabels(yticklabels, fontsize=fstick) 
         if ax.get_subplotspec().is_last_row():
@@ -98,8 +94,6 @@ for ax, traitlabel in zip(axs[1], traitlabels):
 
 for given in givens:
 
-    Z0 = np.full([nr, nc, 4], mymodule.colormap['default'])
-    Z1 = np.full([nr, nc, 4], mymodule.colormap['default'])
     a2eq0 = np.copy(zeros)
     a2eq1 = np.copy(zeros)
     weq0 = np.copy(zeros)
@@ -112,33 +106,41 @@ for given in givens:
     w02 = mymodule.fitness(a20, a22, given, AA, RR)
     w20 = mymodule.fitness(a22, a20, given, AA, RR)
 
+    Z = np.full([nr, nc, 4], mymodule.colormap['default'])
+
     mask0 = (w00 > w11)
     T = np.copy(w01)
     R = np.copy(w00)
     P = np.copy(w11)
     S = np.copy(w10)
-    mymodule.gametypes(mask0, T, R, P, S, a20, a21, Z0, a2eq0, xeq, weq0)
+    mymodule.gametypes(mask0, T, R, P, S, a20, a21, Z, a2eq0, xeq, weq0)
 
     mask0 = (w00 < w11)
     T = np.copy(w10)
     R = np.copy(w11)
     P = np.copy(w00)
     S = np.copy(w01)
-    mymodule.gametypes(mask0, T, R, P, S, a21, a20, Z0, a2eq0, xeq, weq0)
+    mymodule.gametypes(mask0, T, R, P, S, a21, a20, Z, a2eq0, xeq, weq0)
+
+    axs[0, 0].imshow(Z, extent=extent)
+
+    Z = np.full([nr, nc, 4], mymodule.colormap['default'])
 
     mask0 = (w11 > w22)
     T = np.copy(w12)
     R = np.copy(w11)
     P = np.copy(w22)
     S = np.copy(w21)
-    mymodule.gametypes(mask0, T, R, P, S, a21, a22, Z1, a2eq1, xeq, weq1)
+    mymodule.gametypes(mask0, T, R, P, S, a21, a22, Z, a2eq1, xeq, weq1)
 
     mask0 = (w11 < w22)
     T = np.copy(w21)
     R = np.copy(w22)
     P = np.copy(w11)
     S = np.copy(w12)
-    mymodule.gametypes(mask0, T, R, P, S, a22, a21, Z1, a2eq1, xeq, weq1)
+    mymodule.gametypes(mask0, T, R, P, S, a22, a21, Z, a2eq1, xeq, weq1)
+
+    axs[0, 1].imshow(Z, extent=extent)
 
     a2eq = np.copy(a2eq0)
     weq = np.copy(weq0)
@@ -147,12 +149,6 @@ for given in givens:
     weq[mask] = weq1[mask]
 
     Mss = [[a2social, wsocial], [a2eq, weq]]
-
-    Z = np.full([nr*numa2, nc, 4], mymodule.colormap['default'])
-    Z[::2,:] = Z1
-    Z[1::2,:] = Z0
-
-    axs[0, 0].imshow(Z, extent=extenta2, aspect=1.0/numa2)
 
     for row, Ms in zip(axs[1:], Mss):
         for ax, M, traitvmax in zip(row, Ms, traitvmaxs):

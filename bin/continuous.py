e6889e011811c17c0a1b372c6cbbea88fc62900b
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-08 13:00:30 +0100

    Update

--- bin/continuous.py
+++ bin/continuous.py
@@ -11,8 +11,14 @@ import time
 
 start_time = time.perf_counter ()
 
-traits = ['a2Seenmean', 'ChooseGrainmean', 'MimicGrainmean', 'wmean']
-traitlabels = ['Effort to get $\it{B}$', 'Sensitivity for\nchoosing partner', 'Sensitivity for\nmimicking partner', 'Fitness']
+traits = ['a2Seenmean',
+            'ChooseGrainmean',
+            'MimicGrainmean',
+            'wmean']
+traitlabels = ['Effort to get $\it{B}$',
+                'Sensitivity for\nchoosing partner',
+                'Sensitivity for\nmimicking partner',
+                'Fitness']
 folders = ['none', 'p', 'r', 'pr', 'p8r', 'given0']
 
 movie = False
@@ -27,7 +33,8 @@ plt.rcParams['ps.fonttype'] = 42
 
 dfs = []
 for folder in folders:
-    df = pd.concat(map(pd.read_csv, glob(os.path.join(folder, '*.csv'))), ignore_index=True)
+    df = pd.concat(map(pd.read_csv, glob(os.path.join(folder, '*.csv'))),
+                    ignore_index=True)
     df.ChooseGrainmean = 1.0 - df.ChooseGrainmean
     df.MimicGrainmean = 1.0 - df.MimicGrainmean
     #df['help'] = df.a2Seenmean*mymodule.R2*df.Given
@@ -75,14 +82,29 @@ else:
     AA = np.full([nr, nc], alphas[0])
     AAA = np.full([nr*numa2, nc*numa2], alphas[0])
 
-traitvmaxs = [mymodule.a2max, mymodule.a2max, mymodule.a2max, mymodule.fitness(np.array([mymodule.a2max]), np.array([mymodule.a2max]), np.array([0.0]), np.array([0.9]), np.array([5.0]))]
-xticklabels = [round(xmin), round((xmin + xmax)/2), round(xmax)]
-yticklabels = [round(ymin, 1), round((ymin + ymax)/2, 1), round(ymax, 1)]
+traitvmaxs = [mymodule.a2max,
+                mymodule.a2max,
+                mymodule.a2max,
+                mymodule.fitness(np.array([mymodule.a2max]),
+                    np.array([mymodule.a2max]),
+                    np.array([0.0]),
+                    np.array([0.9]),
+                    np.array([5.0]))]
+xticklabels = [round(xmin),
+                round((xmin + xmax)/2),
+                round(xmax)]
+yticklabels = [round(ymin, 1),
+                round((ymin + ymax)/2, 1),
+                round(ymax, 1)]
 extent = 0, nc, 0, nr
 extenta2 = 0, nc*numa2, 0, nr*numa2
 
 MRT0 = mymodule.b*mymodule.Rq
-X, Y = np.meshgrid(np.linspace(0.0, mymodule.a2max, num=numa2), np.linspace(mymodule.a2max, 0.0, num=numa2))
+X, Y = np.meshgrid(np.linspace(0.0,
+                                mymodule.a2max, num=numa2),
+                                np.linspace(mymodule.a2max,
+                                0.0,
+                                num=numa2))
 X = np.tile(A=X, reps=[nr, nc])
 Y = np.tile(A=Y, reps=[nr, nc])
 T = mymodule.fitness(Y, X, GGG, AAA, RRR)
@@ -103,26 +125,44 @@ Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
 a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
 weq = mymodule.fitness(a2eq, a2eq, GG, AA, RR)
 
-fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1)))
+fig, axs = plt.subplots(nrows=len(folders)+1,
+                        ncols=len(traits),
+                        figsize=(6*len(traits), 6*(len(folders)+1)))
 fig.delaxes(axs[0, 0])
 fig.delaxes(axs[0, 3])
-fig.supxlabel(xlabel, x=0.513, y=0.06, fontsize=fslabel*1.50)
-fig.supylabel(ylabel, x=0.05, y=0.493, fontsize=fslabel*1.50, ha='center')
+fig.supxlabel(xlabel,
+                x=0.513,
+                y=0.06,
+                fontsize=fslabel*1.5)
 
 letter = ord('c')
 for axrow in axs:
     for ax in axrow:
         if ax.get_subplotspec().is_first_row():
-            ax.set(xticks=[0, nc*numa2/2, nc*numa2], yticks=[0, nr*numa2/2, nr*numa2], xticklabels=[], yticklabels=[])
+            ax.set(xticks=[0, nc*numa2/2, nc*numa2],
+                    yticks=[0, nr*numa2/2, nr*numa2],
+                    xticklabels=[],
+                    yticklabels=[])
             pos = ax.get_position()
             newpos = [pos.x0, pos.y0+0.04, pos.width, pos.height]
             ax.set_position(newpos)
         else:
-            ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
+            ax.set(xticks=[0, nc/2, nc],
+                    yticks=[0, nr/2, nr],
+                    xticklabels=[],
+                    yticklabels=[])
             if letter<= ord('z'):
-                ax.text(0, nr*1.035, chr(letter), fontsize=fslabel, weight='bold')
+                ax.text(0, 
+                        nr*1.035,
+                        chr(letter),
+                        fontsize=fslabel,
+                        weight='bold')
             else:
-                ax.text(0, nr*1.035, 'a' + chr(letter - 26), fontsize=fslabel, weight='bold')
+                ax.text(0,
+                        nr*1.035,
+                        'a' + chr(letter - 26),
+                        fontsize=fslabel,
+                        weight='bold')
             letter += 1
             if ax.get_subplotspec().is_first_col():
                 ax.set_yticklabels(yticklabels, fontsize=fstick) 
@@ -140,23 +180,46 @@ Z[(T >= R) & (P <= S)] = mymodule.colormap['snowdrift']
 #Z[(T >= R) & (P <= S) & (2.0*R <= T + S)] = mymodule.colormap['snowdriftRS']
 axs[0, 1].imshow(Z, extent=extenta2)
 axs[0, 1].set_title('Snowdrift', pad=50.0, fontsize=fslabel)
-axs[0, 1].text(0, nr*numa2*1.035, 'a', fontsize=fslabel, weight='bold')
+axs[0, 1].text(0,
+                nr*numa2*1.035,
+                'a',
+                fontsize=fslabel,
+                weight='bold')
 axs[0, 1].set_yticklabels(yticklabels, fontsize=fstick) 
 
 TS = np.zeros([nr*numa2, nc*numa2])
 mask = (T > R) & (P > S)
 TS[mask] =  (1.0 + T[mask] + S[mask] - 2.0*R[mask])/2.0 
-axs[0, 2].imshow(TS, extent=extenta2, cmap='magma', vmin=0.0, vmax=0.7)
-axs[0, 2].set_title('2$\it{R}$ - $\it{T}$ - $\it{S}$ under\nprisoner\'s dilemma', pad=50.0, fontsize=fslabel)
-axs[0, 2].text(0, nr*numa2*1.035, 'b', fontsize=fslabel, weight='bold')
+axs[0, 2].imshow(TS, extent=extenta2, cmap='magma', vmin=0.0, vmax=0.5)
+axs[0, 2].set_title('2$\it{R}$ - $\it{T}$ - $\it{S}$ under\nprisoner\'s dilemma',
+                    pad=50.0,
+                    fontsize=fslabel)
+axs[0, 2].text(0,
+                nr*numa2*1.035,
+                'b',
+                fontsize=fslabel,
+                weight='bold')
 
 for t in ts:
     for axrow, df in zip(axs[1:], dfs):
         for ax, trait, traitvmax in zip(axrow, traits, traitvmaxs):
-            Z = pd.pivot_table(df.loc[df.Time == t], values=trait, index=[rowindex], columns=['logES']).sort_index(axis=0, ascending=False)
-            ax.imshow(Z, extent=extent, cmap='magma', vmin=0, vmax=traitvmax)
+            Z = pd.pivot_table(df.loc[df.Time == t],
+                                values=trait,
+                                index=[rowindex],
+                                columns=['logES']).sort_index(axis=0,
+                                                            ascending=False)
+            ax.imshow(Z,
+                    extent=extent,
+                    cmap='magma',
+                    vmin=0,
+                    vmax=traitvmax)
     if movie:
-        text = fig.text(0.90, 0.90, f't\n{t}', fontsize=fstick+4, color='grey', ha='right')
+        text = fig.text(0.90,
+                        0.90,
+                        f't\n{t}',
+                        fontsize=fstick+4,
+                        color='grey',
+                        ha='right')
         plt.savefig('temp.png', transparent=False)
         text.remove()
         frames.append(iio.imread('temp.png'))
--- bin/discrete.py
+++ bin/discrete.py
@@ -11,14 +11,23 @@ import time
 
 start_time = time.perf_counter ()
 
-trait0s = ['nodilemmaRS', 'snowdrift', 'prisoner', 'prisonerRS']
+trait0s = ['nodilemmaRS',
+            'snowdrift',
+            'prisoner',
+            'prisonerRS']
 traitlabel0s = ['Game types', 
                 'Game types', 
                 '2$\it{R}$ - $\it{T}$ - $\it{S}$', 
                 '2$\it{R}$ - $\it{T}$ - $\it{S}$']
-traits = ['a2Seenmean', 'ChooseGrainmean', 'MimicGrainmean', 'wmean']
-traitlabels = ['Effort to get $\it{B}$', 'Sensitivity for\nchoosing partner', 'Sensitivity for\nmimicking partner', 'Fitness']
-folders = ['none', 'p', 'r', 'pr', 'p8r', '../given00/none']
+traits = ['a2Seenmean',
+            'ChooseGrainmean',
+            'MimicGrainmean',
+            'wmean']
+traitlabels = ['Effort to get $\it{B}$',
+                'Sensitivity for\nchoosing partner',
+                'Sensitivity for\nmimicking partner',
+                'Fitness']
+folders = ['none', 'p', 'r', 'pr', 'p8r', 'given0']
 
 movie = False
 
@@ -31,10 +40,11 @@ plt.rcParams['ps.fonttype'] = 42
 
 dfs = []
 for folder in folders:
-    df = pd.concat(map(pd.read_csv, glob(os.path.join(folder, '*.csv'))), ignore_index=True)
+    df = pd.concat(map(pd.read_csv, glob(os.path.join(folder, '*.csv'))),
+                    ignore_index=True)
     df.ChooseGrainmean = 1.0 - df.ChooseGrainmean
     df.MimicGrainmean = 1.0 - df.MimicGrainmean
-    df['help'] = df.a2Seenmean*mymodule.R2*df.Given
+    #df['help'] = df.a2Seenmean*mymodule.R2*df.Given
     dfs.append(df)
 
 df = dfs[0]
@@ -73,9 +83,20 @@ else:
     nr = len(givens)
     AA = np.full([nr, nc], alphas[0])
 
-traitvmaxs = [mymodule.a2max, mymodule.a2max, mymodule.a2max, mymodule.fitness(np.array([mymodule.a2max]), np.array([mymodule.a2max]), np.array([0.0]), np.array([0.9]), np.array([5.0]))]
-xticklabels = [round(xmin), round((xmin + xmax)/2), round(xmax)]
-yticklabels = [round(ymin, 1), round((ymin + ymax)/2, 1), round(ymax, 1)]
+traitvmaxs = [mymodule.a2max,
+                mymodule.a2max,
+                mymodule.a2max,
+                mymodule.fitness(np.array([mymodule.a2max]),
+                    np.array([mymodule.a2max]),
+                    np.array([0.0]),
+                    np.array([0.9]),
+                    np.array([5.0]))]
+xticklabels = [round(xmin),
+                round((xmin + xmax)/2),
+                round(xmax)]
+yticklabels = [round(ymin, 1),
+                round((ymin + ymax)/2, 1),
+                round(ymax, 1)]
 extent = 0, nc, 0, nr
 
 zeros = np.zeros([nr, nc])
@@ -86,19 +107,39 @@ w00 = mymodule.fitness(a20, a20, GG, AA, RR)
 w11 = mymodule.fitness(a21, a21, GG, AA, RR)
 w22 = mymodule.fitness(a22, a22, GG, AA, RR)
 
-fig, axs = plt.subplots(nrows=len(folders)+1, ncols=len(traits), figsize=(6*len(traits), 6*(len(folders)+1)))
-fig.supxlabel(xlabel, x=0.513, y=0.06, fontsize=fslabel*1.5)
-fig.supylabel(ylabel, x=0.05, y=0.493, fontsize=fslabel*1.5, ha='center')
+fig, axs = plt.subplots(nrows=len(folders)+1,
+                        ncols=len(traits),
+                        figsize=(6*len(traits), 6*(len(folders)+1)))
+fig.supxlabel(xlabel,
+                x=0.513,
+                y=0.06,
+                fontsize=fslabel*1.5)
+fig.supylabel(ylabel,
+                x=0.05,
+                y=0.493,
+                fontsize=fslabel*1.5,
+                ha='center')
 
 letter = ord('a')
 for axrow in axs:
     for ax in axrow:
         if letter <= ord('z'): 
-            ax.text(0, nr*1.035, chr(letter), fontsize=fslabel, weight='bold')
+            ax.text(0,
+                    nr*1.035,
+                    chr(letter),
+                    fontsize=fslabel,
+                    weight='bold')
         else:
-            ax.text(0, nr*1.035, 'a' + chr(letter - 26), fontsize=fslabel, weight='bold')
+            ax.text(0,
+                    nr*1.035,
+                    'a' + chr(letter - 26),
+                    fontsize=fslabel,
+                    weight='bold')
         letter += 1
-        ax.set(xticks=[0, nc/2, nc], yticks=[0, nr/2, nr], xticklabels=[], yticklabels=[])
+        ax.set(xticks=[0, nc/2, nc],
+                yticks=[0, nr/2, nr],
+                xticklabels=[],
+                yticklabels=[])
         if ax.get_subplotspec().is_first_row():
             pos = ax.get_position()
             newpos = [pos.x0, pos.y0+0.04, pos.width, pos.height]
@@ -166,10 +207,23 @@ axs[0, 3].imshow(TS, extent=extent, cmap='magma', vmin=0, vmax=0.7)
 for t in ts:
     for axrow, df in zip(axs[1:], dfs):
         for ax, trait, traitvmax in zip(axrow, traits, traitvmaxs):
-            Z = pd.pivot_table(df.loc[df.Time == t], values=trait, index=[rowindex], columns=['logES']).sort_index(axis=0, ascending=False)
-            ax.imshow(Z, extent=extent, cmap='magma', vmin=0, vmax=traitvmax)
+            Z = pd.pivot_table(df.loc[df.Time == t],
+                                values=trait,
+                                index=[rowindex],
+                                columns=['logES']).sort_index(axis=0,
+                                                            ascending=False)
+            ax.imshow(Z,
+                    extent=extent,
+                    cmap='magma',
+                    vmin=0,
+                    vmax=traitvmax)
     if movie:
-        text = fig.text(0.90, 0.90, f't\n{t}', fontsize=fstick+4, color='grey', ha='right')
+        text = fig.text(0.90,
+                        0.90,
+                        f't\n{t}',
+                        fontsize=fstick+4,
+                        color='grey',
+                        ha='right')
         plt.savefig('temp.png', transparent=False)
         text.remove()
         frames.append(iio.imread('temp.png'))

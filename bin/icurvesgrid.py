65ffa89da1027217f266349f998c2b5a4b3800e2
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-13 12:04:13 +0100

    Update

--- bin/icurvesgrid.py
+++ bin/icurvesgrid.py
@@ -30,11 +30,20 @@ every = int(num/2)
 
 def indifference(q, w, alpha, rho):
     if rho == 0.0:
-        q2 = np.piecewise(q, [q == 0.0, q > 0.0], [1000.0, lambda i: pow(w/pow(i, 1.0 - alpha), 1.0/alpha)])
+        q2 = np.piecewise(q,
+                        [q == 0.0,
+                            q > 0.0],
+                        [1000.0,
+                            lambda i: pow(w/pow(i, 1.0 - alpha), 1.0/alpha)])
     elif rho < 0.0:
-        q2 = np.piecewise(q, [q == 0.0, q > 0.0], [1000.0, lambda i: np.piecewise(i, [pow(w, rho) <= (1.0 - alpha)*pow(i, rho), pow(w, rho) > (1.0 - alpha)*pow(i, rho)], [1000.0, lambda j: pow((pow(w, rho) - (1.0 - alpha)*pow(j, rho))/alpha, 1.0/rho)])])
+        q2 = np.piecewise(q,
+                        [q == 0.0,
+                            q > 0.0],
+                        [1000.0,
+                            lambda i: np.piecewise(i, [pow(w, rho) <= (1.0 - alpha)*pow(i, rho), pow(w, rho) > (1.0 - alpha)*pow(i, rho)], [1000.0, lambda j: pow((pow(w, rho) - (1.0 - alpha)*pow(j, rho))/alpha, 1.0/rho)])])
     else:
-        q2 = np.piecewise(q, [pow(w, rho) <= (1.0 - alpha)*pow(q, rho), pow(w, rho) > (1.0 - alpha)*pow(q, rho)], [-0.1, lambda i: pow((pow(w, rho) - (1.0 - alpha)*pow(i, rho))/alpha, 1.0/rho)])
+        q2 = np.piecewise(q,
+                        [pow(w, rho) <= (1.0 - alpha)*pow(q, rho), pow(w, rho) > (1.0 - alpha)*pow(q, rho)], [-0.1, lambda i: pow((pow(w, rho) - (1.0 - alpha)*pow(i, rho))/alpha, 1.0/rho)])
     return q2
 
 nc = num
@@ -55,7 +64,11 @@ wis = np.linspace(2.0/(n_ic + 1), 2.0*n_ic/(n_ic + 1), num=n_ic)
 xlabel = 'Substitutability of $\it{B}$'
 ylabel = 'Value of $\it{B}$'
 
-traitvmax = mymodule.fitness(np.array([mymodule.a2max]), np.array([mymodule.a2max]), np.array([0.0]), np.array([0.9]), np.array([5.0]))
+traitvmax = mymodule.fitness(np.array([mymodule.a2max]),
+                                np.array([mymodule.a2max]),
+                                np.array([0.0]),
+                                np.array([0.9]),
+                                np.array([5.0]))
 icsss = []
 for alpha in alphas:
     icss = []
@@ -68,25 +81,42 @@ for alpha in alphas:
 
 fig = plt.figure(figsize=(len(givens)*6, 6))
 fig.supxlabel(xlabel, x=0.525, y=0.0, fontsize=fslabel)
-fig.supylabel(ylabel, x=0.07, y=0.52, fontsize=fslabel)
-outer_grid = fig.add_gridspec(1, len(givens), left=0.15, right=0.9, top=0.86, bottom=0.176)
+fig.supylabel(ylabel, x=0.08, y=0.52, fontsize=fslabel)
+outer_grid = fig.add_gridspec(1,
+                                len(givens),
+                                left=0.15,
+                                right=0.9,
+                                top=0.86,
+                                bottom=0.176)
 
 letter = ord('a')
 for outer, given in enumerate(givens):
     grid = outer_grid[0, outer].subgridspec(num, num, wspace=0, hspace=0)
     axs = grid.subplots()
-    axs[0, int(num/2)].set_title(f'{given*100:2.0f}%', fontsize=fslabel)
-    axs[0, 0].set_title(chr(letter), fontsize=fslabel, weight='bold', loc='left')
+    axs[0, int(num/2)].set_title(f'{given*100:2.0f}%',
+                                    pad=30.0,
+                                    fontsize=fslabel*0.8)
+    axs[0, 0].set_title(chr(letter),
+                        fontsize=fslabel*0.8,
+                        weight='bold',
+                        loc='left')
     letter += 1
 
     for row in axs:
         for ax in row:
-            ax.set(xticks=[], yticks=[], xlim=(0.0, mymodule.a1max*mymodule.R1), ylim=(0.0, mymodule.a2max*mymodule.R2))
+            ax.set(xticks=[],
+                    yticks=[],
+                    xlim=(0.0, mymodule.a1max*mymodule.R1),
+                    ylim=(0.0, mymodule.a2max*mymodule.R2))
     for ax, loges in zip(axs[-1, ::every], logess[::every]):
         ax.set_xlabel(round(loges), fontsize=fstick)
     if outer == 0:
         for ax, alpha in zip(axs[::every, 0], alphas[::every]):
-            ax.set_ylabel(f'{alpha:1.1f}', rotation='horizontal', horizontalalignment='right', verticalalignment='center', fontsize=fstick)
+            ax.set_ylabel(f'{alpha:1.1f}',
+                            rotation='horizontal',
+                            horizontalalignment='right',
+                            verticalalignment='center',
+                            fontsize=fstick)
 
     MRT = MRT0*(1.0 - given)
     Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
@@ -102,8 +132,12 @@ for outer, given in enumerate(givens):
             for ic in ics:
                 ax.plot(q1_ic, ic, c='0.850')
             budget = budget0 + q2eq*given
-            ax.plot(q1_budget, budget, c='green', alpha=0.8)
-            ax.plot(q1_ic, indifference(q1_ic, weq, alpha, rho), linewidth=4, alpha= 0.8, c=cm.viridis(weq/traitvmax))
+            ax.plot(q1_budget, budget, c='black', alpha=0.8)
+            ax.plot(q1_ic,
+                    indifference(q1_ic, weq, alpha, rho),
+                    linewidth=4,
+                    alpha= 0.8,
+                    c=cm.viridis(weq/traitvmax))
 
 plt.savefig(filename + '.png', transparent=False)
 

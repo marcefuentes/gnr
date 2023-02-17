3842fca7da65ad6c08bb86885d5051aee939fba1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-17 12:07:23 +0100

    Update

--- bin/gamesd.py
+++ bin/gamesd.py
@@ -13,11 +13,12 @@ traitlabels = ['Games',
                 '$\it{T}$ + $\it{S}$ - 2$\it{R}$',
                 'Effort to get $\it{B}$',
                 'Fitness']
-givens = np.linspace(0.0, 1.0, num=2)
-a2lows = [0.50, 0.25, 0.00]
-
-num = 100    # Number of subplot rows and columns
+given = 0.95
+a2lows = np.linspace(0.0, 0.5, num=41)
+title = 'a2: '
 filename = 'gamesd'
+
+num = 1001    # Number of subplot rows and columns
 
 fslabel = 32 # Label font size
 fstick = 18 # Tick font size
@@ -50,67 +51,68 @@ yticklabels = [round(ymin, 1),
                 round(ymax, 1)]
 extent2 = 0, num, 0, num
 
-fig, axs = plt.subplots(nrows=len(a2lows),
+fig, axs = plt.subplots(nrows=1,
                         ncols=len(traitlabels),
-                        figsize=(6*len(traitlabels), 6*(len(a2lows))))
+                        figsize=(6*len(traitlabels), 6))
 fig.supxlabel(xlabel, x=0.513, y=0.01, fontsize=fslabel*1.2)
 fig.supylabel(ylabel, x=0.03, y=0.493, fontsize=fslabel*1.2)
 
 letter = ord('a')
-for axrow in axs:
-    for ax, traitlabel in zip(axrow, traitlabels):
-        ax.text(0, 
-                num*1.035,
-                chr(letter),
-                fontsize=fslabel*0.8,
-                weight='bold')
-        letter += 1
-        ax.set(xticks=[0, num/2, num],
-                yticks=[0, num/2, num],
-                xticklabels=[],
-                yticklabels=[])
-        if ax.get_subplotspec().is_first_col():
-            ax.set_yticklabels(yticklabels, fontsize=fstick) 
-        if ax.get_subplotspec().is_first_row():
-            ax.set_title(traitlabel, pad=40.0, fontsize=fslabel*0.9)
-        if ax.get_subplotspec().is_last_row():
-            ax.set_xticklabels(xticklabels, fontsize=fstick)
+for ax, traitlabel in zip(axs, traitlabels):
+    ax.text(0, 
+            num*1.035,
+            chr(letter),
+            fontsize=fslabel*0.8,
+            weight='bold')
+    letter += 1
+    ax.set(xticks=[0, num/2, num],
+            yticks=[0, num/2, num],
+            xticklabels=[],
+            yticklabels=[])
+    if ax.get_subplotspec().is_first_col():
+        ax.set_yticklabels(yticklabels, fontsize=fstick) 
+    ax.set_title(traitlabel, pad=40.0, fontsize=fslabel*0.9)
+    ax.set_xticklabels(xticklabels, fontsize=fstick)
 
 frames = []
-for given in givens:
+for a2low in a2lows:
 
-    for axrow, a2low in zip(axs, a2lows):
+    low = np.full([num, num], a2low)
+    high = low + 0.5
+    T = mymodule.fitness(high, low, given, AA, RR)
+    R = mymodule.fitness(high, high, given, AA, RR)
+    P = mymodule.fitness(low, low, given, AA, RR)
+    S = mymodule.fitness(low, high, given, AA, RR)
+    Z = np.full([num, num, 4], mymodule.colormap['red'])
+    mymodule.gamecolors(T, R, P, S, Z)
+    axs[0].imshow(Z, extent=extent2)
 
-        low = np.full([num, num], a2low)
-        high = np.full([num, num], a2low + 0.5)
-        T = mymodule.fitness(high, low, given, AA, RR)
-        R = mymodule.fitness(high, high, given, AA, RR)
-        P = mymodule.fitness(low, low, given, AA, RR)
-        S = mymodule.fitness(low, high, given, AA, RR)
-        Z = np.full([num, num, 4], mymodule.colormap['red'])
-        mymodule.gamecolors(T, R, P, S, Z)
-        axrow[0].imshow(Z, extent=extent2)
+    Z = np.zeros([num, num])
+    mask = mymodule.prisoner(T, R, P, S) & (2.0*R > T + S)
+    Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
+    Z = np.ma.masked_where(Z == 0.0, Z)
+    cmap = plt.cm.viridis
+    cmap.set_bad(color='white')
+    axs[1].imshow(Z, extent=extent2, cmap=cmap, vmin=0, vmax=traitvmaxs[0])
 
-        a2eq = np.zeros([num, num])
-        weq = np.zeros([num, num])
-        mymodule.equilibrium(T, R, P, S, low, high, a2eq, weq)
-        if given == 0.0:
-            mask = (R == P)
-            a2eq[mask] = (low[mask] + high[mask])/2.0
-            weq[mask] = (mymodule.fitness(low[mask], low[mask], given, AA[mask], RR[mask]) +
-                            mymodule.fitness(high[mask], high[mask], given, AA[mask], RR[mask]))/2.0
+    a2eq = np.zeros([num, num])
+    weq = np.zeros([num, num])
+    mymodule.equilibrium(T, R, P, S, low, high, a2eq, weq)
+    if given == 0.0:
+        mask = (R == P)
+        a2eq[mask] = (low[mask] + high[mask])/2.0
+        weq[mask] = (mymodule.fitness(low[mask], low[mask], given, AA[mask], RR[mask]) +
+                        mymodule.fitness(high[mask], high[mask], given, AA[mask], RR[mask]))/2.0
+    axs[2].imshow(a2eq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[0])
+    axs[3].imshow(weq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[1])
 
-        Z = np.zeros([num, num])
-        mask = mymodule.prisoner(T, R, P, S) & (2.0*R > T + S)
-        Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
-        Z = np.ma.masked_where(Z == 0.0, Z)
-        cmap = plt.cm.viridis
-        cmap.set_bad(color='white')
-        axrow[1].imshow(Z, extent=extent2, cmap=cmap, vmin=0, vmax=traitvmaxs[0])
-        axrow[2].imshow(a2eq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[0])
-        axrow[3].imshow(weq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[1])
-
-    text = fig.text(0.90, 0.90, f'{given:4.2f}', fontsize=fstick, color='grey', ha='right')
+    movieframe = a2low
+    text = fig.text(0.90,
+                    0.02,
+                    title + f'{movieframe:4.2f}',
+                    fontsize=fslabel,
+                    color='grey',
+                    ha='right')
     plt.savefig('temp.png', transparent=False)
     text.remove()
     frames.append(iio.imread('temp.png'))

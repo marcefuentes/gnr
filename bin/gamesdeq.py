dec5b6bf28a70585b0df41b5eaee1acda05614ee
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-18 19:13:12 +0100

    Update

--- bin/gamesdeq.py
+++ bin/gamesdeq.py
@@ -16,6 +16,7 @@ traitlabels = ['Games (lower)',
                 'Games (upper)',
                 '$\it{T}$ + $\it{S}$ - 2$\it{R}$']
 givens = np.linspace(0.0, 1.0, num=20)
+givens = np.linspace(0.95, 0.95, num=1)
 title = 'Given: '
 
 num = 1001    # Number of subplot rows and columns
@@ -25,8 +26,8 @@ fstick = 18 # Tick font size
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
-if given > 0.9999999:
-    given = 0.9999999
+if givens[-1] > 0.9999999:
+    givens[-1] = 0.9999999
 alphas = np.linspace(mymodule.alphamax, mymodule.alphamin, num=num)
 logess = np.linspace(mymodule.logesmin, mymodule.logesmax, num=num)
 rhos = 1.0 - 1.0/pow(2, logess)
@@ -34,11 +35,6 @@ RR, AA = np.meshgrid(rhos, alphas)
 MRT0 = mymodule.b*mymodule.Rq
 Q0 = mymodule.Rq*pow(MRT0*AA/(1.0 - AA), 1.0/(RR - 1.0))
 a2social = mymodule.a2max/(1.0 + Q0*mymodule.b)
-MRT = MRT0*(1.0 - given)
-Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
-a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
-a2lows = [0.0, a2eq]
-a2highs = [a2social, mymodule.a2max]
 
 xmin = logess[0]
 xmax = logess[-1]
@@ -79,38 +75,58 @@ for ax, traitlabel in zip(axs, traitlabels):
     ax.set_xticklabels(xticklabels, fontsize=fstick)
 
 frames = []
-for i, (a2low, a2high) in enumerate(zip(a2lows, a2highs)):
+for given in givens:
 
-    low = np.full([num, num], a2low)
-    high = np.full([num, num], a2high)
-    T = mymodule.fitness(high, low, given, AA, RR)
-    R = mymodule.fitness(high, high, given, AA, RR)
-    P = mymodule.fitness(low, low, given, AA, RR)
-    S = mymodule.fitness(low, high, given, AA, RR)
-    Z = np.full([num, num, 4], mymodule.colormap['red'])
-    mymodule.gamecolors(T, R, P, S, Z)
-    axs[2*i].imshow(Z, extent=extent2)
+    MRT = MRT0*(1.0 - given)
+    Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
+    a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
+    a2lows = [0.0, a2eq]
+    a2highs = [a2social, mymodule.a2max]
 
-    if i == 0:
-        Z = np.zeros([num, num])
-        mask = (T > R) & (R > P) & (P > S)
-        Z[mask] = R[mask] - P[mask]
-        Z = np.ma.masked_where(Z == 0.0, Z)
-        cmap = plt.cm.viridis
-        cmap.set_bad(color='white')
-        axs[2*i+1].imshow(Z, extent=extent2, cmap=cmap)
-    else:
-        Z = np.zeros([num, num])
-        mask = mymodule.prisoner(T, R, P, S) & (2.0*R > T + S)
-        Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
-        Z = np.ma.masked_where(Z == 0.0, Z)
-        cmap = plt.cm.viridis
-        cmap.set_bad(color='white')
-        axs[2*i+1].imshow(Z, extent=extent2, cmap=cmap)
+    for i, (a2low, a2high) in enumerate(zip(a2lows, a2highs)):
+        low = np.full([num, num], a2low)
+        high = np.full([num, num], a2high)
+        T = mymodule.fitness(high, low, given, AA, RR)
+        R = mymodule.fitness(high, high, given, AA, RR)
+        P = mymodule.fitness(low, low, given, AA, RR)
+        S = mymodule.fitness(low, high, given, AA, RR)
+        Z = np.full([num, num, 4], mymodule.colormap['red'])
+        mymodule.gamecolors(T, R, P, S, Z)
+        axs[2*i].imshow(Z, extent=extent2)
 
-plt.savefig(filename + '.png', transparent=False)
+        if i == 0:
+            Z = np.zeros([num, num])
+            #mask = (T > R) & (R > P) & (P > S)
+            mask = (R >= P)
+            Z[mask] = R[mask] - P[mask]
+            Z = np.ma.masked_where(Z == 0.0, Z)
+            cmap = plt.cm.viridis
+            cmap.set_bad(color='white')
+            axs[2*i+1].imshow(Z, extent=extent2, cmap=cmap)
+        else:
+            Z = np.zeros([num, num])
+            mask = mymodule.prisoner(T, R, P, S) | (mymodule.deadlock(T, R, P, S) & (2.0*P < T + S)) | (R == P)
+            Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
+            Z = np.ma.masked_where(Z == 0.0, Z)
+            cmap = plt.cm.viridis
+            cmap.set_bad(color='white')
+            axs[2*i+1].imshow(Z, extent=extent2, cmap=cmap)
+
+    movieframe = given
+    text = fig.text(0.90,
+                    0.02,
+                    title + f'{movieframe:4.2f}',
+                    fontsize=fslabel,
+                    color='grey',
+                    ha='right')
+    plt.savefig('temp.png', transparent=False)
+    text.remove()
+    frames.append(iio.imread('temp.png'))
+    os.remove('temp.png')
 
 plt.close()
+
+iio.mimsave(filename + '.gif', frames)
 
 end_time = time.perf_counter ()
 print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')

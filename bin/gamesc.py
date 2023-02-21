7760ab257c88afe99ad6405ac003a13462310b90
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-21 20:15:45 +0100

    Update

--- bin/gamesc.py
+++ bin/gamesc.py
@@ -10,113 +10,111 @@ start_time = time.perf_counter ()
 thisscript = os.path.basename(__file__)
 filename = thisscript.split('.')[0]
 
-titles = ['Games',
-                '$\it{T}$ + $\it{S}$ - 2$\it{R}$',
-                '$\it{S}$ - $\it{P}$']
-given = 0.95
-
-num = 1001
-numg = 21
-numa2 = 64
+givens = [0.5, 0.95]
+num = 5    # Number of subplot rows & columns
+numa2 = 512
 
 fslarge = 32 # Label font size
 fssmall = 18 # Tick font size
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
-if given > 0.9999999:
-    given = 0.9999999
+if givens[-1] > 0.9999999:
+    givens[-1] = 0.9999999
 alphas = np.linspace(mymodule.alphamax, mymodule.alphamin, num=num)
 logess = np.linspace(mymodule.logesmin, mymodule.logesmax, num=num)
 rhos = 1.0 - 1.0/pow(2, logess)
 RR, AA = np.meshgrid(rhos, alphas)
 MRT0 = mymodule.b*mymodule.Rq
 Q0 = mymodule.Rq*pow(MRT0*AA/(1.0 - AA), 1.0/(RR - 1.0))
-#a2social = mymodule.a2max/(1.0 + Q0*mymodule.b)
-MRT = MRT0*(1.0 - given)
-Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
-a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
-weq = mymodule.fitness(a2eq, a2eq, given, AA, RR)
+a2social = mymodule.a2max/(1.0 + Q0*mymodule.b)
+xmin = 0.0
+xmax = mymodule.a2max
+ymin = 0.0
+ymax = mymodule.a2max
 
-X, Y = np.meshgrid(np.linspace(0.0, mymodule.a2max, num=numa2),
-                    np.linspace(mymodule.a2max, 0.0, num=numa2))
-X = np.tile(A=X, reps=[numg, numg])
-Y = np.tile(A=Y, reps=[numg, numg])
-H = np.copy(Y)
-Y[(X > Y)] = X[(X > Y)] 
-X[(X > H)] = H[(X > H)] 
-alphas = np.linspace(mymodule.alphamax, mymodule.alphamin, num=numg)
-logess = np.linspace(mymodule.logesmin, mymodule.logesmax, num=numg)
-rhos = 1.0 - 1.0/pow(2, logess)
-RRR, AAA = np.meshgrid(np.repeat(rhos, numa2),
-                        np.repeat(alphas, numa2))
-T = mymodule.fitness(Y, X, given, AAA, RRR)
-R = mymodule.fitness(Y, Y, given, AAA, RRR)
-P = mymodule.fitness(X, X, given, AAA, RRR)
-S = mymodule.fitness(X, Y, given, AAA, RRR)
+X, Y = np.meshgrid(np.linspace(xmin, xmax, num=numa2),
+                    np.linspace(ymax, ymin, num=numa2))
 
-xmin = logess[0]
-xmax = logess[-1]
+every = int(num/2)
 xlabel = 'Substitutability of $\it{B}$'
-ymin = alphas[-1]
-ymax = alphas[0]
 ylabel = 'Value of $\it{B}$'
-
-xticklabels = [round(xmin),
-                round((xmin + xmax)/2),
-                round(xmax)]
-yticklabels = [round(ymin, 1),
-                round((ymin + ymax)/2, 1),
-                round(ymax, 1)]
-extent2 = 0, num, 0, num
-extentg = 0, numg*numa2, 0, numg*numa2
-
-fig, axs = plt.subplots(nrows=1,
-                        ncols=len(titles),
-                        figsize=(6*len(titles), 6))
-fig.supxlabel(xlabel, x=0.513, y=0.01, fontsize=fslarge*1.2)
-fig.supylabel(ylabel, x=0.03, y=0.493, fontsize=fslarge*1.2)
-
 letter = ord('a')
-for ax, title in zip(axs, titles):
-    ax.text(0, 
-            numg*numa2*1.035,
-            chr(letter),
-            fontsize=fslarge*0.8,
-            weight='bold')
+extent2 = 0, numa2, 0, numa2
+
+fig = plt.figure(figsize=(6*len(givens), 6))
+fig.supxlabel(xlabel,
+                x=0.525,
+                y=0.0,
+                fontsize=fslarge)
+fig.supylabel(ylabel,
+                x=0.05,
+                y=0.52,
+                fontsize=fslarge)
+
+outergrid = fig.add_gridspec(nrows=1,
+                                ncols=len(givens),
+                                left=0.15,
+                                right=0.9,
+                                top=0.86,
+                                bottom=0.176)
+
+for outer, given in zip(outergrid, givens):
+    grid = outer.subgridspec(nrows=num,
+                                ncols=num,
+                                wspace=0,
+                                hspace=0)
+    axs = grid.subplots()
+    axs[0, int(num/2)].set_title(f'{given*100:2.0f}%',
+                                    pad=30.0,
+                                    fontsize=fslarge*0.8)
+    axs[0, 0].set_title(chr(letter),
+                        fontsize=fslarge*0.8,
+                        weight='bold',
+                        loc='left')
     letter += 1
-    ax.set_title(title, pad=40.0, fontsize=fslarge*0.9)
-    ax.set(xticks=[0, numg*numa2/2, numg*numa2],
-            yticks=[0, numg*numa2/2, numg*numa2],
-            xticklabels=[])
-    ax.set_xticklabels(xticklabels, fontsize=fssmall)
-    if ax.get_subplotspec().is_first_col():
-        ax.set_yticklabels(yticklabels, fontsize=fssmall) 
-    else:
-        ax.set_yticklabels([]) 
 
-Z = np.full([numg*numa2, numg*numa2, 4], mymodule.colormap['white'])
-mymodule.gamecolors(T, R, P, S, Z)
-axs[0].imshow(Z, extent=extentg)
+    for i in range(num):
+        for j in range(num):
+            axs[i, j].set(xticks=[],
+                    yticks=[])
+    for ax, loges in zip(axs[-1, ::every], logess[::every]):
+        ax.set_xlabel(f'{loges:2.0f}', fontsize=fssmall)
+    if given == 0.5:
+        for ax, alpha in zip(axs[::every, 0], alphas[::every]):
+            ax.set_ylabel(f'{alpha:3.1f}',
+                            rotation='horizontal',
+                            horizontalalignment='right',
+                            verticalalignment='center',
+                            fontsize=fssmall)
 
-Z = np.zeros([numg*numa2, numg*numa2])
-mask = (mymodule.prisoner(T, R, P, S) & (2.0*R < T + S)) | (mymodule.deadlock(T, R, P, S) & (2.0*P < T + S))
-Z[mask] = 0.1 - (2.0*R[mask] - T[mask] - S[mask])
-Z = np.ma.masked_where(Z == 0.0, Z)
-cmap = plt.cm.viridis
-cmap.set_bad(color='white')
-axs[1].imshow(Z, extent=extentg, cmap=cmap)
+    MRT = MRT0*(1.0 - given)
+    Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
+    a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
 
-Z = np.zeros([numg*numa2, numg*numa2])
-mask = mymodule.prisoner(T, R, P, S)
-Z[mask] = 1.0 - (S[mask] - P[mask])
-Z = np.ma.masked_where(Z == 0.0, Z)
-cmap = plt.cm.viridis
-cmap.set_bad(color='white')
-axs[2].imshow(Z, extent=extentg, cmap=cmap)
-
-#axs[2].imshow(a2eq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[0])
-#axs[3].imshow(weq, extent=extent2, cmap='viridis', vmin=0, vmax=traitvmaxs[1])
+    for i in range(num): 
+        for j in range(num):
+            AAA = np.full([numa2, numa2], alphas[i])
+            RRR = np.full([numa2, numa2], rhos[j])
+            T = mymodule.fitness(Y, X, given, AAA, RRR)
+            R = mymodule.fitness(Y, Y, given, AAA, RRR)
+            P = mymodule.fitness(X, X, given, AAA, RRR)
+            S = mymodule.fitness(X, Y, given, AAA, RRR)
+            Z = np.full([numa2, numa2, 4], mymodule.colormap['white'])
+            mymodule.gamecolors(T, R, P, S, Z)
+            mask = (X > Y)
+            Z[mask] = [0.9, 0.9, 0.9, 1.0]
+            axs[i, j].imshow(Z, extent=extent2)
+            axs[i, j].plot(0.01*a2eq[i, j]*numa2,
+                        (0.99*a2social[i, j] + 0.01*mymodule.a2max)*numa2,
+                        marker='o',
+                        color='orange',
+                        markersize=4)
+            axs[i, j].plot(0.99*a2eq[i, j]*numa2,
+                        (0.01*a2social[i, j] + 0.99*mymodule.a2max)*numa2,
+                        marker='o',
+                        color='red',
+                        markersize=4)
 
 plt.savefig(filename + '.png', transparent=False)
 

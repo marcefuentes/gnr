6698cdaeed6a8ac1cbdd03cbfe31b5118209f4c2
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-23 19:47:13 +0100

    Update

--- bin/discrete.py
+++ bin/discrete.py
@@ -22,12 +22,8 @@ traits = ['ChooseGrainmean',
             'MimicGrainmean']
 folders = ['a2init75', 'a2init50', 'a2init25']
 subfolder = 'pr'
-a2lows = np.array([0.50, 0.25, 0.00])
-a2highs = a2lows + 0.5
 
 movie = False
-
-num = 21
 
 fslarge = 32 # Label font size
 fssmall = 24 # Tick font size
@@ -54,52 +50,34 @@ rowindex = 'alpha'
 logess = np.sort(pd.unique(df.logES))
 nr = len(alphas)
 nc = len(logess)
-alphas = np.linspace(alphas[0], alphas[-1], num=num)
-logess = np.linspace(logess[0], logess[-1], num=num)
 rhos = 1.0 - 1.0/pow(2.0, logess)
 RR, AA = np.meshgrid(rhos, alphas)
-MRT0 = mymodule.b*mymodule.Rq
-Q0 = mymodule.Rq*pow(MRT0*AA/(1.0 - AA), 1.0/(RR - 1.0))
-a2social = mymodule.a2max/(1.0 + Q0*mymodule.b)
-MRT = MRT0*(1.0 - given)
-Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
-a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
-lows = [np.full([num, num], a2lows[0]),
-        np.full([num, num], a2lows[1]),
-        np.full([num, num], a2lows[2])]
-highs = [np.full([num, num], a2highs[0]), 
-        np.full([num, num], a2highs[1]), 
-        np.full([num, num], a2highs[2])] 
 
 xlabel = 'Substitutability of $\it{B}$'
 ylabel = 'Value of $\it{B}$'
 letter = ord('a')
-letterposition = num*1.035
-letterpositionr = nr*1.035
+letterposition = nr*1.035
 xmin = logess[0]
 xmax = logess[-1]
 ymin = alphas[-1]
 ymax = alphas[0]
-xticks = [0, num/2, num]
-yticks = [0, num/2, num]
-xticksr = [0, nc/2, nc]
-yticksr = [0, nr/2, nr]
+xticks = [0, nc/2, nc]
+yticks = [0, nr/2, nr]
 xticklabels = [f'{xmin:2.0f}',
                 f'{(xmin + xmax)/2.0:2.0f}',
                 f'{xmax:2.0f}']
 yticklabels = [f'{ymin:3.1f}',
                 f'{(ymin + ymax)/2.0:3.1f}',
                 f'{ymax:3.1f}']
-extentnum = 0, num, 0, num
-extentr= 0, nc, 0, nr
+extent= 0, nc, 0, nr
 cmap = plt.cm.viridis
 cmap.set_bad(color='white')
 traitvmaxs = [mymodule.a2max,
                 mymodule.a2max]
 
-fig, axs = plt.subplots(nrows=len(highs),
+fig, axs = plt.subplots(nrows=len(dfs),
                         ncols=len(titles),
-                        figsize=(6*len(titles), 6*len(highs)))
+                        figsize=(6*len(titles), 6*len(dfs)))
 fig.supxlabel(xlabel,
                 x=0.513,
                 y=0.02,
@@ -109,29 +87,18 @@ fig.supylabel(ylabel,
                 y=0.493,
                 fontsize=fslarge*1.3)
 
-for i in range(len(highs)):
+for i in range(len(dfs)):
     for j, title in enumerate(titles):
         ax = axs[i, j]
-        if 'Sensitivity' not in title:
-            ax.set(xticks=xticks,
-                    yticks=yticks,
-                    xticklabels=[],
-                    yticklabels=[])
-            ax.text(0,
-                    letterposition,
-                    chr(letter),
-                    fontsize=fslarge*0.8,
-                    weight='bold')
-        else:
-            ax.set(xticks=xticksr,
-                    yticks=yticksr,
-                    xticklabels=[],
-                    yticklabels=[])
-            ax.text(0,
-                    letterpositionr,
-                    chr(letter),
-                    fontsize=fslarge*0.8,
-                    weight='bold')
+        ax.set(xticks=xticks,
+                yticks=yticks,
+                xticklabels=[],
+                yticklabels=[])
+        ax.text(0,
+                letterposition,
+                chr(letter),
+                fontsize=fslarge*0.8,
+                weight='bold')
         letter += 1
         if ax.get_subplotspec().is_first_row():
             ax.set_title(title, pad=40.0, fontsize=fslarge)
@@ -140,27 +107,39 @@ for i in range(len(highs)):
         if ax.get_subplotspec().is_last_row():
             ax.set_xticklabels(xticklabels, fontsize=fssmall)
 
-for i, (low, high) in enumerate(zip(lows, highs)):
+for i, df in enumerate(dfs):
 
-    T = mymodule.fitness(high, low, given, AA, RR)
-    R = mymodule.fitness(high, high, given, AA, RR)
-    P = mymodule.fitness(low, low, given, AA, RR)
-    S = mymodule.fitness(low, high, given, AA, RR)
-    Z = np.full([num, num, 4], mymodule.colormap['red'])
+    lows = pd.pivot_table(df.loc[df.Time == 1],
+                values='a2low',
+                index=[rowindex],
+                columns=['logES']).sort_index(axis=0,
+                                            ascending=False)
+    lows = lows.to_numpy()
+    highs = pd.pivot_table(df.loc[df.Time == 1],
+                values='a2high',
+                index=[rowindex],
+                columns=['logES']).sort_index(axis=0,
+                                            ascending=False)
+    highs = highs.to_numpy()
+    T = mymodule.fitness(highs, lows, given, AA, RR)
+    R = mymodule.fitness(highs, highs, given, AA, RR)
+    P = mymodule.fitness(lows, lows, given, AA, RR)
+    S = mymodule.fitness(lows, highs, given, AA, RR)
+    Z = np.full([nr, nc, 4], mymodule.colormap['red'])
     mymodule.gamecolors(T, R, P, S, Z)
-    axs[i, 0].imshow(Z, extent=extentnum)
+    axs[i, 0].imshow(Z, extent=extent)
 
-    Z = np.zeros([num, num])
+    Z = np.zeros([nr, nc])
     mask = mymodule.dilemma(T, R, P, S)
     Z[mask] = R[mask] - P[mask] + 0.000001
     Z = np.ma.masked_where(Z == 0.0, Z)
-    axs[i, 1].imshow(Z, extent=extentnum, cmap=cmap, vmin=-1, vmax=1)
+    axs[i, 1].imshow(Z, extent=extent, cmap=cmap, vmin=-1, vmax=1)
 
-    Z = np.zeros([num, num])
+    Z = np.zeros([nr, nc])
     mask = mymodule.dilemma(T, R, P, S)
     Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
     Z = np.ma.masked_where(Z == 0.0, Z)
-    axs[i, 2].imshow(Z, extent=extentnum, cmap=cmap, vmin=-1, vmax=1)
+    axs[i, 2].imshow(Z, extent=extent, cmap=cmap, vmin=-1, vmax=1)
 
 for t in ts:
     for i, df in enumerate(dfs):
@@ -171,7 +150,7 @@ for t in ts:
                         columns=['logES']).sort_index(axis=0,
                                                     ascending=False)
             axs[i, j + 3].imshow(Z,
-                                extent=extentr,
+                                extent=extent,
                                 vmin=0,
                                 vmax=vmax)
     if movie:

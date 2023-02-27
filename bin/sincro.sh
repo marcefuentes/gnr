a85336d407002fe273267e2cdf10390f9590bdcf
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-02-27 10:12:41 +0100

    Update

--- bin/sincro.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source_folder="$HOME/code/gnr/results/" # source
-destination_folder="$STORE/gnr/results/" # destination
-
-rsync --archive --verbose --progress --exclude='*.out' $source_folder $destination_folder 
-

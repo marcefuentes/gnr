04157947cbf828af0c96ef24fd3a107ffdf7a8e5
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-03-02 19:45:27 +0100

    Update

---
+++ bin/oldcontinuous.py
@@ -0,0 +1,215 @@
+#! /usr/bin/env python
+
+from glob import glob
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+import mymodule
+import numpy as np
+import os
+import pandas as pd
+import time
+
+start_time = time.perf_counter ()
+thisscript = os.path.basename(__file__)
+filename = thisscript.split('.')[0]
+
+titles = ['Games (lower)',
+                '$\it{R}$ - $\it{P}$',
+                'Games (upper)',
+                '$\it{T}$ + $\it{S}$ - 2$\it{R}$']
+traits = ['a2Seenmean',
+            'ChooseGrainmean',
+            'MimicGrainmean',
+            'wmean']
+titletraits = ['Effort to get $\it{B}$',
+                'Sensitivity for\nchoosing partner',
+                'Sensitivity for\nmimicking partner',
+                'Fitness']
+folders = ['given0', 'none', 'p', 'r', 'pr', 'p8r']
+
+movie = False
+
+num = 21
+
+fslarge = 32 # Label font size
+fssmall = 24 # Tick font size
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+dfs = []
+for folder in folders:
+    df = pd.concat(map(pd.read_csv, glob(os.path.join(folder, '*.csv'))),
+                    ignore_index=True)
+    df.ChooseGrainmean = 1.0 - df.ChooseGrainmean
+    df.MimicGrainmean = 1.0 - df.MimicGrainmean
+    dfs.append(df)
+
+df = dfs[1]
+ts = df.Time.unique()
+if movie:
+    frames = []
+else:
+    ts = [ts[-1]]
+given = df.Given[0]
+if given > 0.9999999:
+    given = 0.9999999
+alphas = np.sort(pd.unique(df.alpha))[::-1]
+rowindex = 'alpha'
+logess = np.sort(pd.unique(df.logES))
+nr = len(alphas)
+nc = len(logess)
+alphas = np.linspace(alphas[0], alphas[-1], num=num)
+logess = np.linspace(logess[0], logess[-1], num=num)
+rhos = 1.0 - 1.0/pow(2.0, logess)
+RR, AA = np.meshgrid(rhos, alphas)
+MRT0 = mymodule.b*mymodule.Rq
+Q0 = mymodule.Rq*pow(MRT0*AA/(1.0 - AA), 1.0/(RR - 1.0))
+a2social = mymodule.a2max/(1.0 + Q0*mymodule.b)
+MRT = MRT0*(1.0 - given)
+Q = mymodule.Rq*pow(MRT*AA/(1.0 - AA), 1.0/(RR - 1.0))
+a2eq = mymodule.a2max/(1.0 + Q*mymodule.b)
+lows = [np.full([num, num], 0.0),
+        np.full([num, num], a2eq)]
+highs = [np.full([num, num], a2social),
+        np.full([num, num], mymodule.a2max)]
+
+xlabel = 'Substitutability of $\it{B}$'
+ylabel = 'Value of $\it{B}$'
+letter = ord('a')
+letterposition = num*1.035
+letterpositionr = nr*1.035
+xmin = logess[0]
+xmax = logess[-1]
+ymin = alphas[-1]
+ymax = alphas[0]
+xticks = [0, num/2, num]
+yticks = [0, num/2, num]
+xticksr = [0, nc/2, nc]
+yticksr = [0, nr/2, nr]
+xticklabels = [f'{xmin:2.0f}',
+                f'{(xmin + xmax)/2.0:2.0f}',
+                f'{xmax:2.0f}']
+yticklabels = [f'{ymin:3.1f}',
+                f'{(ymin + ymax)/2.0:3.1f}',
+                f'{ymax:3.1f}']
+extentnum = 0, num, 0, num
+extentr= 0, nc, 0, nr
+cmap = plt.cm.viridis
+cmap.set_bad(color='white')
+traitvmaxs = [mymodule.a2max,
+                mymodule.a2max,
+                mymodule.a2max,
+                mymodule.fitness(np.array([mymodule.a2max]),
+                                    np.array([mymodule.a2max]),
+                                    np.array([0.0]),
+                                    np.array([0.9]),
+                                    np.array([5.0]))]
+
+fig, axs = plt.subplots(nrows=len(folders)+1,
+                        ncols=len(traits),
+                        figsize=(6*len(traits), 6*(len(folders)+1)))
+fig.supxlabel(xlabel,
+                x=0.513,
+                y=0.06,
+                fontsize=fslarge*1.5)
+fig.supylabel(ylabel,
+                x=0.05,
+                y=0.493,
+                fontsize=fslarge*1.5)
+
+for i in range(len(folders) + 1):
+    for j in range(len(traits)):
+        ax = axs[i, j]
+        if letter <= ord('z'): 
+            textl = chr(letter)
+        else:
+            textl = 'a' + chr(letter - 26)
+        letter += 1
+        if ax.get_subplotspec().is_first_row():
+            pos = ax.get_position()
+            newpos = [pos.x0, pos.y0+0.04, pos.width, pos.height]
+            ax.set_position(newpos)
+            ax.set(xticks=xticks,
+                    yticks=yticks,
+                    xticklabels=[],
+                    yticklabels=[])
+            ax.text(0,
+                    letterposition,
+                    textl,
+                    fontsize=fslarge,
+                    weight='bold')
+        else:
+            ax.set(xticks=xticksr,
+                    yticks=yticksr,
+                    xticklabels=[],
+                    yticklabels=[])
+            ax.text(0,
+                    letterpositionr,
+                    textl,
+                    fontsize=fslarge,
+                    weight='bold')
+        if ax.get_subplotspec().is_first_col():
+            ax.set_yticklabels(yticklabels, fontsize=fssmall) 
+        if ax.get_subplotspec().is_last_row():
+            ax.set_xticklabels(xticklabels, fontsize=fssmall)
+for j, title in enumerate(titles):
+    axs[0, j].set_title(title, pad=50.0, fontsize=fslarge)
+for j, title in enumerate(titletraits):
+    axs[1, j].set_title(title, pad=50.0, fontsize=fslarge)
+
+for i, (low, high) in enumerate(zip(lows, highs)):
+
+    T = mymodule.fitness(high, low, given, AA, RR)
+    R = mymodule.fitness(high, high, given, AA, RR)
+    P = mymodule.fitness(low, low, given, AA, RR)
+    S = mymodule.fitness(low, high, given, AA, RR)
+    Z = np.full([num, num, 4], mymodule.colormap['red'])
+    mymodule.gamecolors(T, R, P, S, Z)
+    axs[0, 2*i].imshow(Z, extent=extentnum)
+
+    if i == 0:
+        Z = np.zeros([num, num])
+        mask = mymodule.dilemma(T, R, P, S)
+        Z[mask] = R[mask] - P[mask] + 0.000001
+        Z = np.ma.masked_where(Z == 0.0, Z)
+        axs[0, 2*i+1].imshow(Z, extent=extentnum, cmap=cmap, vmin=-1, vmax=1)
+    else:
+        Z = np.zeros([num, num])
+        mask = mymodule.dilemma(T, R, P, S)
+        Z[mask] = 1.0 - (2.0*R[mask] - T[mask] - S[mask])
+        Z = np.ma.masked_where(Z == 0.0, Z)
+        axs[0, 2*i+1].imshow(Z, extent=extentnum, cmap=cmap, vmin=-1, vmax=1)
+
+for t in ts:
+    for axrow, df in zip(axs[1:], dfs):
+        for ax, trait, traitvmax in zip(axrow, traits, traitvmaxs):
+            Z = pd.pivot_table(df.loc[df.Time == t],
+                                values=trait,
+                                index=[rowindex],
+                                columns=['logES']).sort_index(axis=0,
+                                                            ascending=False)
+            ax.imshow(Z,
+                    extent=extentr,
+                    vmin=0,
+                    vmax=traitvmax)
+    if movie:
+        text = fig.text(0.90,
+                        0.90,
+                        f't\n{t}',
+                        fontsize=fssmall+4,
+                        color='grey',
+                        ha='right')
+        plt.savefig('temp.png', transparent=False)
+        text.remove()
+        frames.append(iio.imread('temp.png'))
+        os.remove('temp.png')
+    else:
+        plt.savefig(filename + '.png', transparent=False)
+
+plt.close()
+
+if movie:
+    iio.mimsave(filename + '.gif', frames)
+
+end_time = time.perf_counter ()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')
---
+++ bin/saddlepoints.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python
+
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import mymodule
+import numpy as np
+import os
+import time
+
+start_time = time.perf_counter()
+thisscript = os.path.basename(__file__)
+filename = thisscript.split('.')[0]
+
+given = 0.95
+num = 5    # Number of subplot rows & columns
+ext = 256
+distances = np.array([0.2, 0.5, 0.8])
+
+plotsize = 9
+
+alphas = np.linspace(mymodule.alphamax, mymodule.alphamin, num=num)
+logess = np.linspace(mymodule.logesmin, mymodule.logesmax, num=num)
+rhos = 1.0 - 1.0/pow(2, logess)
+
+step = int(num/2)
+xlabel = 'Substitutability of $\it{B}$'
+ylabel = 'Value of $\it{B}$'
+extent = 0, ext, 0, ext
+markersize = plotsize*4
+width = plotsize
+height = plotsize
+biglabels = plotsize*4
+ticklabels = plotsize*3
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+fig = plt.figure(figsize=(width, height))
+fig.supxlabel(xlabel,
+              x=0.55,
+              y=0.05,
+              fontsize=biglabels)
+fig.supylabel(ylabel,
+              x=0.04,
+              y=0.55,
+              fontsize=biglabels)
+
+grid = fig.add_gridspec(nrows=num,
+                        ncols=num,
+                        wspace=0,
+                        hspace=0,
+                        left=0.20,
+                        right=0.9,
+                        top=0.9,
+                        bottom=0.20)
+axs = grid.subplots()
+for ax in fig.get_axes():
+    ax.set(xticks=[], yticks=[])
+    #ax.set(xlim=xlim, ylim=ylim)
+for i in range(0, num, step):
+    axs[i, 0].set_ylabel(f'{alphas[i]:3.1f}',
+                         rotation='horizontal',
+                         horizontalalignment='right',
+                         verticalalignment='center',
+                         fontsize=ticklabels)
+    axs[-1, i].set_xlabel(f'{logess[i]:2.0f}',
+                          fontsize=ticklabels)
+
+for i, alpha in enumerate(alphas):
+    AA = np.full([ext, ext], alpha)
+    for j, rho in enumerate(rhos):
+        a2eq = mymodule.a2eq(given, alpha, rho)
+        a2social = mymodule.a2eq(0.0, alpha, rho)
+        x = distances*a2eq
+        y = a2social + distances*(mymodule.a2max - a2social)
+        
+        T = mymodule.fitness(y, x, given, alpha, rho)
+        R = mymodule.fitness(y, y, given, alpha, rho)
+        P = mymodule.fitness(x, x, given, alpha, rho)
+        S = mymodule.fitness(x, y, given, alpha, rho)
+
+        Z = mymodule.gamecolors(T, R, P, S)
+        axs[i][j].scatter(distances*ext, distances*ext,
+                          marker='o',
+                          s=markersize,
+                          color=Z)
+        Z = mymodule.nodilemmacolorsg(T, R, P, S)
+        axs[i][j].scatter(distances*ext, distances*ext,
+                          marker='o',
+                          s=markersize,
+                          color=Z)
+
+        x = np.linspace(0.0, a2eq, num=ext)
+        y = np.linspace(mymodule.a2max, a2social, num=ext)
+        X, Y = np.meshgrid(x, y)
+        RR = np.full([ext, ext], rho)
+        T = mymodule.fitness(Y, X, given, AA, RR)
+        R = mymodule.fitness(Y, Y, given, AA, RR)
+        P = mymodule.fitness(X, X, given, AA, RR)
+        S = mymodule.fitness(X, Y, given, AA, RR)
+
+        Z = mymodule.gamecolors(T, R, P, S)
+        axs[i][j].imshow(Z, extent=extent, alpha=0.2)
+
+plt.savefig(filename + '.png', transparent=False)
+
+plt.close()
+
+end_time = time.perf_counter()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')

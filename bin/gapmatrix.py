2b1e1f81d8cfaadcce7407231a12578675d19cfd
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-03-07 20:10:15 +0100

    Update

---
+++ bin/gapmatrix.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python
+
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+import mymodule
+import numpy as np
+import os
+import time
+
+start_time = time.perf_counter()
+thisscript = os.path.basename(__file__)
+filename = thisscript.split('.')[0]
+
+titles = ['Games',
+          '$\it{R}$ - $\it{P}$',
+          '$\it{T}$ + $\it{S}$ - 2$\it{R}$']
+givens = [0.5, 0.95, 1.0]
+distances = [0.8, 0.5, 0.2]
+rows = distances
+ext = 512
+plotsize = 4
+
+alphas = np.linspace(mymodule.alphamax, mymodule.alphamin, num=ext)
+logess = np.linspace(mymodule.logesmin, mymodule.logesmax, num=ext)
+rhos = 1.0 - 1.0/pow(2, logess)
+RR, AA = np.meshgrid(rhos, alphas)
+highs = [] 
+eq = mymodule.a2eq(0.0, AA, RR)
+for distance in distances:
+    highs.append(eq + distance*(mymodule.a2max - eq))
+
+xlabel = 'Substitutability of $\it{B}$'
+ylabel = 'Value of $\it{B}$'
+letter = ord('a')
+letterposition = 1.035
+xticks = [-0.5, ext/2-0.5, ext-0.5]
+yticks = [-0.5, ext/2-0.5, ext-0.5]
+xmin = logess[0]
+xmax = logess[-1]
+ymin = alphas[-1]
+ymax = alphas[0]
+xticklabels = [f'{xmin:2.0f}',
+               f'{(xmin + xmax)/2.0:2.0f}',
+               f'{xmax:2.0f}']
+yticklabels = [f'{ymax:3.1f}',
+               f'{(ymin + ymax)/2.0:3.1f}',
+               f'{ymin:3.1f}']
+width = plotsize*len(titles)
+height = plotsize*len(rows)
+biglabels = plotsize*5 + height/4
+ticklabels = plotsize*4
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+fig, axs = plt.subplots(nrows=len(rows),
+                        ncols=len(titles),
+                        figsize=(width, height))
+fig.supxlabel(xlabel,
+              x=0.513,
+              y=0.01,
+              fontsize=biglabels)
+fig.supylabel(ylabel,
+              x=0.03,
+              y=0.493,
+              fontsize=biglabels)
+
+for ax in fig.get_axes():
+    ax.set(xticks=xticks, yticks=yticks)
+    ax.set(xticklabels=[], yticklabels=[])
+    ax.text(0,
+            letterposition,
+            chr(letter),
+            transform=ax.transAxes,
+            fontsize=plotsize*5,
+            weight='bold')
+    letter += 1
+for i, row in enumerate(rows):
+    axs[i, 0].set_yticklabels(yticklabels, fontsize=ticklabels)
+for j, title in enumerate(titles):
+    axs[0, j].set_title(title, pad=plotsize*10, fontsize=plotsize*5)
+    axs[-1, j].set_xticklabels(xticklabels, fontsize=ticklabels)
+
+frames = []
+for given in givens:
+
+    eq = mymodule.a2eq(given, AA, RR)
+    for i, distance in enumerate(distances):
+
+        low = distance*eq
+        high = highs[i]
+
+        T = mymodule.fitness(high, low, given, AA, RR)
+        R = mymodule.fitness(high, high, given, AA, RR)
+        P = mymodule.fitness(low, low, given, AA, RR)
+        S = mymodule.fitness(low, high, given, AA, RR)
+
+        Z = mymodule.gamecolors(T, R, P, S)
+        axs[i, 0].imshow(Z)
+
+        N = mymodule.nodilemmacolors(T, R, P, S)
+
+        Z = R - P
+        axs[i, 1].imshow(Z, vmin=-1, vmax=1)
+        axs[i, 1].imshow(N)
+
+        Z = T + S - 2.0*R
+        mask = R < P
+        Z[mask] = T[mask] + S[mask] - 2.0*P[mask]
+        axs[i, 2].imshow(Z, vmin=-1, vmax=1)
+        axs[i, 2].imshow(N)
+
+    text = fig.text(0.90,
+                    0.02,
+                    'Given: ' + f'{given:4.2f}',
+                    fontsize=biglabels,
+                    color='grey',
+                    ha='right')
+    plt.savefig('temp.png', transparent=False)
+    text.remove()
+    frames.append(iio.imread('temp.png'))
+    os.remove('temp.png')
+
+plt.close()
+
+iio.mimsave(filename + '.gif', frames)
+
+end_time = time.perf_counter()
+print(f'\nTime elapsed: {(end_time - start_time):.2f} seconds')

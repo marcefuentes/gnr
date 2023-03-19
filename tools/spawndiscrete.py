f9728a5f07e0d160bd6f1d21f425a9c22259cdfd
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-03-20 00:18:48 +0100

    When Discrete=1, read a2 values from *.glo

--- tools/spawndiscrete.py
+++ tools/spawndiscrete.py
@@ -1,15 +1,41 @@
 #! /usr/bin/env python
 
+from glob import glob
+import os
 import numpy as np
+import pandas as pd
 
+folders = ['none', 'given0']
+given = 0.95
 num = 21
-givens = np.linspace(0.0, 1.0, num)
 alphas = np.linspace(0.1, 0.9, num)
-loges = 1.0
+logess = np.linspace(-5.0, 5.0, num)
 c = 101
 
-for alpha in alphas:
-    for given in givens:
+dfs = np.empty(2, dtype=object)
+for i, folder in enumerate(folders):
+    filelist = glob(os.path.join(folder, '*.csv'))
+    dfs[i] = pd.concat(map(pd.read_csv, filelist),
+                       ignore_index=True)
+
+df = dfs[0]
+ts = df.Time.unique()
+t = ts[-1]
+Zs = np.empty((2, 21, 21), dtype=np.float32)
+
+for i, folder in enumerate(folders):
+    df = dfs[i]
+    m = df.Time == t
+    df = df.loc[m]
+    Z = pd.pivot_table(df,
+                       values='a2Seenmean',
+                       index='alpha',
+                       columns=['logES'])
+    Z = Z.sort_index(axis=0, ascending=True)
+    Zs[i] = Z.to_numpy()
+
+for a, alpha in enumerate(alphas):
+    for l, loges in enumerate(logess):
         filename = str(c) + '.glo'
         f = open(filename, 'w')
 
@@ -34,8 +60,10 @@ for alpha in alphas:
         f.write('PartnerChoice,0\n')
         f.write('Reciprocity,0\n')
         f.write('Discrete,1\n')
+        f.write(f'a2low,{Zs[0, a, l]}\n')
+        f.write(f'a2high,{Zs[1, a, l]}\n')
         f.write('IndirectR,0\n')
-        f.write(f'alpha,{alpha}\n')
+        f.write(f'alpha,{alpha:.6}\n')
         f.write(f'logES,{loges}\n')
         f.write(f'Given,{given}\n')
 

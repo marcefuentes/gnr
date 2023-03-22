f121becc9f8226549bd8bf6af71409ba9ed64955
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-03-22 23:58:07 +0100

    Update

--- tools/spawndiscrete.py
+++ tools/spawndiscrete.py
@@ -5,34 +5,28 @@ import os
 import numpy as np
 import pandas as pd
 
-folders = ['none', 'given0']
 given = 0.95
 num = 21
 alphas = np.linspace(0.1, 0.9, num)
 logess = np.linspace(-5.0, 5.0, num)
 c = 101
 
-dfs = np.empty(2, dtype=object)
-for i, folder in enumerate(folders):
-    filelist = glob(os.path.join(folder, '*.csv'))
-    dfs[i] = pd.concat(map(pd.read_csv, filelist),
+filelist = glob(os.path.join('../given95/none', '*.csv'))
+df = pd.concat(map(pd.read_csv, filelist),
                        ignore_index=True)
 
-df = dfs[0]
 ts = df.Time.unique()
 t = ts[-1]
 Zs = np.empty((2, 21, 21), dtype=np.float32)
 
-for i, folder in enumerate(folders):
-    df = dfs[i]
-    m = df.Time == t
-    df = df.loc[m]
-    Z = pd.pivot_table(df,
-                       values='a2Seenmean',
-                       index='alpha',
-                       columns=['logES'])
-    Z = Z.sort_index(axis=0, ascending=True)
-    Zs[i] = Z.to_numpy()
+m = df.Time == t
+df = df.loc[m]
+Z = pd.pivot_table(df,
+                   values='a2Seenmean',
+                   index='alpha',
+                   columns=['logES'])
+Z = Z.sort_index(axis=0, ascending=True)
+Z = Z.to_numpy()
 
 for a, alpha in enumerate(alphas):
     for l, loges in enumerate(logess):
@@ -60,8 +54,8 @@ for a, alpha in enumerate(alphas):
         f.write('PartnerChoice,0\n')
         f.write('Reciprocity,0\n')
         f.write('Discrete,1\n')
-        f.write(f'a2low,{Zs[0, a, l]}\n')
-        f.write(f'a2high,{Zs[1, a, l]}\n')
+        f.write(f'a2low,{Z[a, l]}\n')
+        f.write(f'a2high,{Z[a, l] + 0.001}\n')
         f.write('IndirectR,0\n')
         f.write(f'alpha,{alpha:.6}\n')
         f.write(f'logES,{loges}\n')

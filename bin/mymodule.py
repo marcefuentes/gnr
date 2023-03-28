c4efc0bf85b60c68b623adfd58ec058f2044abc0
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-03-28 11:24:28 +0200

    Update

--- bin/mymodule.py
+++ bin/mymodule.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import numpy as np
+import pandas as pd
 
 R1 = 2.0
 R2 = 2.0
@@ -206,7 +207,7 @@ def indifference(qs, w, alpha, rho):
                 q2[i] = pow((A - B)/alpha, 1.0/rho)
     return q2
 
-def my.read_file(file, alltimes):
+def read_file(file, alltimes):
     df = pd.read_csv(file)
     if not alltimes:
         df = df.tail(1)

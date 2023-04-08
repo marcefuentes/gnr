0bfdb0f03a6de25a587eed7738e29b57c5c26217
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-08 16:54:40 +0200

    Update

--- bin/discgames.py
+++ bin/discgames.py
@@ -4,7 +4,7 @@ from glob import glob
 import os
 import time
 
-from matplotlib.animation import FuncAnimation
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import matplotlib.transforms
 import numpy as np
@@ -24,66 +24,34 @@ titles = ['Games',
           'Severity of\nsocial dilemma', 
           'Sensitivity for\nchoosing partner',
           'Sensitivity for\nmimicking partner']
-vmaxs = [my.wmax, my.a2max, my.a2max]
 folders = ['given100', 'given95', 'given50']
 subfolders = ['p', 'r']
 
-movie = False
 plotsize = 6
-
-# Add data to figure
-
-def init(artists):
-    for f, folder in enumerate(folders):
-        lows = my.getZ(ts[0], dftraits[f, 0], 'a2low')
-        highs = my.getZ(ts[0], dftraits[f, 0], 'a2high')
-        given = dftraits[f, 0].Given.iloc[0]
-        T = my.fitness(highs, lows, given, AA, RR)
-        R = my.fitness(highs, highs, given, AA, RR)
-        P = my.fitness(lows, lows, given, AA, RR)
-        S = my.fitness(lows, highs, given, AA, RR)
-        Z = my.gamecolors(T, R, P, S)
-        artists[f, 0].set_array(Z)
-    return artists.flatten()
-
-def update(t, artists):
-    Zsocial = my.getZ(t, dfsocial, 'wmean')
-    for f, folder in enumerate(folders):
-        Z = my.getZ(t, dfprivates[f], 'wmean')
-        Z = Zsocial - Z       
-        artists[f, 0].set_array(Z)
-        for c, trait in enumerate(traits):
-            Z = my.getZ(t, dftraits[f, c], trait)
-            if 'Grain' in trait:
-                Z = 1.0 - Z
-            artists[f, c + 1].set_array(Z) 
-    if movie:
-        fig.texts[2].set_text(f't\n{t}')
-    return artists.flatten()
 
 # Data
 
-filelist = glob(os.path.join('given00', 'none', '*.csv'))
-dfsocial = my.read_files(filelist, movie)
-
 dfprivates = np.empty(len(folders), dtype=object)
+dfsocials = np.empty(len(folders), dtype=object)
 for f, folder in enumerate(folders):
     filelist = glob(os.path.join(folder, 'none', '*.csv'))
-    dfprivates[f] = my.read_files(filelist, movie)
+    dfprivates[f] = my.read_files(filelist, False)
+    filelist = glob(os.path.join(folder, 'given0', '*.csv'))
+    dfsocials[f] = my.read_files(filelist, False)
 
 dftraits = np.empty((len(folders), len(subfolders)), dtype=object)
 for f, folder in enumerate(folders):
     for c, subfolder in enumerate(subfolders):
         filelist = glob(os.path.join(folder, subfolder, '*.csv'))
-        dftraits[f, c] = my.read_files(filelist, movie)
+        dftraits[f, c] = my.read_files(filelist, False)
 
 df = dftraits[0, 0]
-ts = df.Time.unique()
+t = df.Time.max()
 alphas = np.sort(pd.unique(df.alpha))[::-1]
 logess = np.sort(pd.unique(df.logES))
 nr = len(alphas)
 nc = len(logess)
-rhos = 1.0 - 1.0/pow(2.0, logess)
+rhos = 1. - 1./pow(2., logess)
 RR, AA = np.meshgrid(rhos, alphas)
 
 # Figure properties
@@ -92,20 +60,22 @@ width = plotsize*len(titles)
 height = plotsize*len(folders)
 xlabel = 'Substitutability of $\it{B}$'
 ylabel = 'Value of $\it{B}$'
-biglabels = plotsize*5 + height/4
-ticklabels = plotsize*4
+biglabel = plotsize*7
+midlabel = plotsize*6
+letterlabel = plotsize*5
+ticklabel = plotsize*4
 xticks = [0, nc/2 - 0.5, nc - 1]
 yticks = [0, nr/2 - 0.5, nr - 1]
 xmin = logess[0]
 xmax = logess[-1]
 ymin = alphas[-1]
 ymax = alphas[0]
-xticklabels = [f'{xmin:2.0f}',
-               f'{(xmin + xmax)/2.0:2.0f}',
-               f'{xmax:2.0f}']
-yticklabels = [f'{ymax:3.1f}',
-               f'{(ymin + ymax)/2.0:3.1f}',
-               f'{ymin:3.1f}']
+xticklabels = [f'{xmin:.0f}',
+               f'{(xmin + xmax)/2.:.0f}',
+               f'{xmax:.0f}']
+yticklabels = [f'{ymax:.1f}',
+               f'{(ymin + ymax)/2:.1f}',
+               f'{ymin:.1f}']
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
@@ -115,82 +85,82 @@ fig, axs = plt.subplots(nrows=len(folders),
                         ncols=len(titles),
                         figsize=(width, height))
 
+
+# Apply properties
+
 left_x = axs[0, 0].get_position().x0
 right_x = axs[-1, -1].get_position().x1
-center_x = (left_x + right_x) / 2
+center_x = (left_x + right_x)/2.
 top_y = axs[0, 0].get_position().y1
 bottom_y = axs[-1, -1].get_position().y0
-center_y = (top_y + bottom_y) / 2
+center_y = (top_y + bottom_y)/2.
 fig.supxlabel(xlabel,
               x=center_x,
-              y=bottom_y*0.5,
-              fontsize=biglabels)
+              y=bottom_y*0.3,
+              fontsize=biglabel)
 fig.supylabel(ylabel,
               x=left_x*0.4,
               y=center_y,
-              fontsize=biglabels)
+              fontsize=biglabel)
 
-dx = -3/72.; dy = 0/72.
-offset = matplotlib.transforms.ScaledTranslation(dx, dy, fig.dpi_scale_trans)
+ox = -3/72.
+oy = 0/72.
+offset = matplotlib.transforms.ScaledTranslation(ox,
+                                                 oy,
+                                                 fig.dpi_scale_trans)
 
+letter = ord('a')
 letterposition = 1.035
+
 for i, ax in enumerate(fig.get_axes()):
     ax.set(xticks=xticks, yticks=yticks)
     ax.set(xticklabels=[], yticklabels=[])
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(0.1)
     letter = ord('a') + i
     ax.text(0,
             letterposition,
             chr(letter),
             transform=ax.transAxes,
-            fontsize=plotsize*5,
+            fontsize=letterlabel,
             weight='bold')
 for f, folder in enumerate(folders):
-    axs[f, 0].set_yticklabels(yticklabels, fontsize=ticklabels)
+    axs[f, 0].set_yticklabels(yticklabels, fontsize=ticklabel)
 for c, title in enumerate(titles):
-    axs[0, c].set_title(title, pad=plotsize*9, fontsize=plotsize*5)
-    axs[-1, c].set_xticklabels(xticklabels, fontsize=ticklabels)
+    axs[0, c].set_title(title, pad=plotsize*9, fontsize=midlabel)
+    axs[-1, c].set_xticklabels(xticklabels, fontsize=ticklabel)
     for label in axs[-1, c].xaxis.get_majorticklabels():
         label.set_transform(label.get_transform() + offset)
-if movie:
-    fig.text(right_x,
-             bottom_y*0.5,
-             f't\n0',
-             fontsize=biglabels,
-             color='grey',
-             ha='right')
 
-# Assign axs objects to variables
-# (AxesImage)
-
-artists = np.empty_like(axs) 
-dummy_Zg = np.empty((nr, nc, 4), dtype=np.float32)
-dummy_Z = np.empty_like(AA)
-frames = ts
-frame0 = ts[-1]
+# Add data
 
 for f, folder in enumerate(folders):
+    lows = my.getZ(t, dftraits[f, 0], 'a2low')
+    highs = my.getZ(t, dftraits[f, 0], 'a2high')
+    given = dftraits[f, 0].Given.iloc[0]
+    T = my.fitness(highs, lows, given, AA, RR)
+    R = my.fitness(highs, highs, given, AA, RR)
+    P = my.fitness(lows, lows, given, AA, RR)
+    S = my.fitness(lows, highs, given, AA, RR)
+    Z = my.gamecolors(T, R, P, S)
+    axs[f, 0].imshow(Z)
 
-    artists[f] = axs[f, 0].imshow(dummy_Zg)
+    Zsocial = my.getZ(t, dfsocials[f], 'wmean')
+    Z = my.getZ(t, dfprivates[f], 'wmean')
+    Z = Zsocial - Z       
+    axs[f, 1].imshow(Z)
 
     for c, trait in enumerate(traits):
-        artists[f, c + 1] = axs[f, c + 1].imshow(dummy_Z,
-                                                 vmin=0,
-                                                 vmax=vmaxs[c])
+        Z = my.getZ(t, dftraits[f, c], trait)
+        if 'Grain' in trait:
+            Z = 1.0 - Z
+        axs[f, c + 2].imshow(Z,
+                             vmin=0,
+                             vmax=my.a2max) 
 
-# Add data and save figure
+# Finish
 
-init(artists,)
-
-if movie:
-    ani = FuncAnimation(fig,
-                        update,
-                        frames=frames,
-                        fargs=(artists,),
-                        blit=True)
-    ani.save(filename + '.mp4', writer='ffmpeg', fps=10)
-else:
-    update(frame0, artists,)
-    plt.savefig(filename + '.png', transparent=False)
+plt.savefig(filename + '.png', transparent=False)
 
 plt.close()
 

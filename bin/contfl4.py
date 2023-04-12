7f7c3290b5b8e6ae56d43c56432f9b93913c4816
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-12 10:00:52 +0200

    Update

--- bin/contfl4.py
+++ bin/contfl4.py
@@ -217,18 +217,20 @@ for f, folder in enumerate(folders):
 
             R = my.fitness(xaxis, xaxis, given, alpha, rho)
 
-            y = my.fitness(xaxis, a2s, given, alpha, rho)
-            color = cm.viridis((my.wmax - y[0])/my.wmax)
-            ax = axlines[f, 0, a, e]
-            #ax.plot(xaxis, R, color='black', linewidth=0.7)
-            ax.plot(xaxis, y, color='red', linewidth=0.7)
-            #ax.set_facecolor(color)
-
             y = my.fitness(a2s, xaxis, given, alpha, rho)
             color = cm.viridis((wss[a, e] - wps[a, e])/my.wmax)
+            ax = axlines[f, 0, a, e]
+            #ax.plot(xaxis, R, color='black', linewidth=0.7)
+            ax.plot(xaxis, y, color='green', linewidth=0.7)
+            ax.plot(a2privates[a, e], wps[a, e], marker='o', markersize=2, color='green')
+            #ax.set_facecolor(color)
+
+            y = my.fitness(xaxis, xaxis, given, alpha, rho)
+            color = cm.viridis((my.wmax - y[0])/my.wmax)
             ax = axlines[f, 1, a, e]
             #ax.plot(xaxis, R, color='black', linewidth=0.7)
-            ax.plot(xaxis, y, color='red', linewidth=0.7)
+            ax.plot(xaxis, y, color='green', linewidth=0.7)
+            ax.plot(a2privates[a, e], wps[a, e], marker='o', markersize=2, color='green')
             #ax.set_facecolor(color)
 
     for c, trait in enumerate(traits):

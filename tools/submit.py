0b92434d2a1879b44f37ea4373de500a3a500198
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-22 15:41:50 +0200

    Update

--- tools/submit.py
+++ tools/submit.py
@@ -2,52 +2,47 @@
 
 import os
 import subprocess
-import sys
 
-# Get the queue name from the command line argument
-queue = sys.argv[1]
+queues = ['epyc', 'clk']
+batchlims = [200, 400]
+job_min = 100
+job_max = 541
+filename = "last_job_submitted.tmp"
 
-# Define the batch limit based on the queue
-batchlim = 400 if queue == "clk" else 200
+last_job = job_min
+if os.path.isfile(filename):
+    with open(filename, "r") as f:
+        last_job = int(f.read().strip())
 
-# Find the number of running and queued jobs in the queue
-output = subprocess.check_output(f"squeue -t RUNNING,PENDING -r -o '%j' | grep -E '^{queue}' | wc -l", shell=True)
-num_jobs_in_queue = int(output.decode().strip())
-available_slots = batchlim - num_jobs_in_queue
-print(f"{num_jobs_in_queue} jobs in {queue}")
-print(f"{available_slots} slots available in {queue}")
+for queue, batchlim in zip(queues, batchlims):
 
-# Find last job submitted
-last_job_submitted = 100
-if os.path.isfile("last_job_submitted.txt"):
-    with open("last_job_submitted.txt", "r") as f:
-        last_job_submitted = int(f.read().strip())
+    output = subprocess.check_output(f"squeue -t RUNNING,PENDING -r -o '%j' | grep -E '^{queue}' | wc -l", shell=True)
+    num_jobs_in_queue = int(output.decode().strip())
+    print(f"{num_jobs_in_queue} jobs in {queue}")
+    available_slots = batchlim - num_jobs_in_queue
+    print(f"{available_slots} slots available in {queue}")
 
-# Submit jobs
-num_jobs_to_submit = min(available_slots, 541 - last_job_submitted)
-if num_jobs_to_submit > 0:
-    job_name = f"{queue}-{os.getcwd().split('/')[-1]}"
-    first_job = last_job_submitted + 1
-    last_job = last_job_submitted + num_jobs_to_submit
-    array = f"{first_job}-{last_job}"
-    subprocess.run(["sbatch",
-                    "--job-name", job_name,
-                    "--output", f"{job_name}.%j.out",
-                    "-C", queue,
-                    "--array", array,
-                    "job.sh"])
-    print(f"with jobs {first_job} to {last_job}")
+    num_jobs_to_submit = min(available_slots, job_max - last_job)
+    if num_jobs_to_submit > 0:
+        job_name = f"{queue}-{os.getcwd().split('/')[-1]}"
+        first_job = last_job + 1
+        last_job = last_job + num_jobs_to_submit
+        array = f"{first_job}-{last_job}"
+        subprocess.run(["sbatch",
+                        "--job-name", job_name,
+                        "--output", f"{job_name}.%j.out",
+                        "-C", queue,
+                        "--array", array,
+                        "job.sh"])
+        print(f"with jobs {first_job} to {last_job}")
+    else:
+        print("No jobs to submit")
+
+    available_slots = available_slots - num_jobs_to_submit
+    print(f"{available_slots} slots still available in {queue}")
+
+if last_job == job_max:
+    os.remove(filename)
 else:
-    print("No jobs to submit")
-
-# Update last_job_submitted.txt
-if last_job == 541:
-    os.remove("last_job_submitted.txt")
-else:
-    with open("last_job_submitted.txt", "w") as f:
+    with open(filename, "w") as f:
         f.write(str(last_job))
-
-# Print the number of available slots in the batch queue
-available_slots = available_slots - num_jobs_to_submit
-print(f"{available_slots} slots still available in {queue}")
-

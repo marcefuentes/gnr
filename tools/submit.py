cf477618042f486ec60cc1fa521d0e38921a5441
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-22 18:54:18 +0200

    Update

--- tools/submit.py
+++ tools/submit.py
@@ -4,25 +4,34 @@ import os
 import subprocess
 
 queues = ['epyc', 'clk']
-batchlims = [200, 400]
+maxsubmits = [200, 400]
 job_min = 100
 job_max = 541
-filename = "last_job_submitted.tmp"
+job_file = "last_job_submitted.tmp"
+folder_file = "/home/ulc/ba/mfu/code/gnr/results/active_folder.tmp"
 
-last_job = job_min
-if os.path.isfile(filename):
-    with open(filename, "r") as f:
+if os.path.isfile(folder_file):
+    with open(folder_file, "r") as f:
+        path = f.read().strip()
+    os.chdir(path)
+    with open(job_file, "r") as f:
         last_job = int(f.read().strip())
+else:
+    last_job = job_min
+    cwd = os.getcwd()
+    with open(folder_file, "w") as f:
+        f.write(cwd)
 
-for queue, batchlim in zip(queues, batchlims):
+for queue, maxsubmit in zip(queues, maxsubmits):
 
+    print(f"\n\033[96m{queue}:\033[0m")
     output = subprocess.check_output(f"squeue -t RUNNING,PENDING -r -o '%j' | grep -E '^{queue}' | wc -l", shell=True)
     num_jobs_in_queue = int(output.decode().strip())
-    print(f"{num_jobs_in_queue} jobs in {queue}")
-    available_slots = batchlim - num_jobs_in_queue
-    print(f"{available_slots} slots available in {queue}")
-
+    print(f"{num_jobs_in_queue} jobs in queue")
+    available_slots = maxsubmit - num_jobs_in_queue
+    print(f"{available_slots} slots available")
     num_jobs_to_submit = min(available_slots, job_max - last_job)
+
     if num_jobs_to_submit > 0:
         job_name = f"{queue}-{os.getcwd().split('/')[-1]}"
         first_job = last_job + 1
@@ -39,10 +48,13 @@ for queue, batchlim in zip(queues, batchlims):
         print("No jobs to submit")
 
     available_slots = available_slots - num_jobs_to_submit
-    print(f"{available_slots} slots still available in {queue}")
+    print(f"{available_slots} slots still available")
+
+print("")
 
 if last_job == job_max:
-    os.remove(filename)
+    os.remove(job_file)
+    os.remove(folder_file)
 else:
-    with open(filename, "w") as f:
+    with open(job_file, "w") as f:
         f.write(str(last_job))

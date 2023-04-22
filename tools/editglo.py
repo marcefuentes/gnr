2668265c6c52fa0d9fdd5dd2c653d0213a449554
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-22 08:44:05 +0200

    Update

---
+++ tools/editglo.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import os
+import fileinput
+import sys
+
+pattern = '*.glo'
+abs_folder_path = os.path.abspath(os.getcwd())
+parent_folder = os.path.basename(os.path.dirname(abs_folder_path))
+folder_name = os.path.basename(abs_folder_path)
+decimal_part = parent_folder[-2:]
+given = float(decimal_part) / 100
+
+for line in fileinput.input(files=[os.path.join(abs_folder_path, f) for f in os.listdir(abs_folder_path) if f.endswith('.glo')], inplace=True, backup='',):
+    sys.stdout.write(line.replace('Given,0.0', f'Given,{given}'))
+
+if 'p' in folder_name:
+    for line in fileinput.input(files=[os.path.join(abs_folder_path, f) for f in os.listdir(abs_folder_path) if f.endswith('.glo')], inplace=True, backup='',):
+        sys.stdout.write(line.replace('PartnerChoice,0', 'PartnerChoice,1'))
+
+if 'r' in folder_name:
+    for line in fileinput.input(files=[os.path.join(abs_folder_path, f) for f in os.listdir(abs_folder_path) if f.endswith('.glo')], inplace=True, backup='',):
+        sys.stdout.write(line.replace('Reciprocity,0', 'Reciprocity,1'))
+
+if '8' in folder_name:
+    for line in fileinput.input(files=[os.path.join(abs_folder_path, f) for f in os.listdir(abs_folder_path) if f.endswith('.glo')], inplace=True, backup='',):
+        sys.stdout.write(line.replace('GroupSize,2', 'Groupsize,3'))
+
---
+++ tools/submit.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+# Get the queue name from the command line argument
+queue = sys.argv[1]
+
+# Define the batch limit based on the queue
+batchlim = 400 if queue == "clk" else 200
+
+# Find the number of running and queued jobs in the queue
+output = subprocess.check_output(f"squeue -t RUNNING,PENDING -r -o '%j' | grep -E '^{queue}' | wc -l", shell=True)
+num_jobs_in_queue = int(output.decode().strip())
+available_slots = batchlim - num_jobs_in_queue
+print(f"{num_jobs_in_queue} jobs in {queue}")
+print(f"{available_slots} slots available in {queue}")
+
+# Find last job submitted
+last_job_submitted = 100
+if os.path.isfile("last_job_submitted.txt"):
+    with open("last_job_submitted.txt", "r") as f:
+        last_job_submitted = int(f.read().strip())
+
+# Submit jobs
+num_jobs_to_submit = min(available_slots, 541 - last_job_submitted)
+if num_jobs_to_submit > 0:
+    job_name = f"{queue}-{os.getcwd().split('/')[-1]}"
+    first_job = last_job_submitted + 1
+    last_job = last_job_submitted + num_jobs_to_submit
+    array = f"{first_job}-{last_job}"
+    subprocess.run(["sbatch",
+                    "--job-name", job_name,
+                    "--output", f"{job_name}.%j.out",
+                    "-C", queue,
+                    "--array", array,
+                    "job.sh"])
+    print(f"with jobs {first_job} to {last_job}")
+else:
+    print("No jobs to submit")
+
+# Update last_job_submitted.txt
+with open("last_job_submitted.txt", "w") as f:
+    f.write(str(last_job))
+
+# Print the number of available slots in the batch queue
+available_slots = available_slots - num_jobs_to_submit
+print(f"{available_slots} slots still available in {queue}")
+

cccb42db773a5de9f5c589eb3d99bfb545c3351f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-04-26 08:00:16 +0200

    Update

--- tools/submit.py
+++ tools/submit.py
@@ -3,24 +3,39 @@
 import os
 import subprocess
 
+folders = ['none', 'p', 'p8', 'p8r', 'pr', 'r']
 queues = ['epyc', 'clk']
 maxsubmits = [200, 400]
 job_min = 100
 job_max = 541
-job_file = "last_job_submitted.tmp"
+job_file = "last_submitted_job.tmp"
 folder_file = "/home/ulc/ba/mfu/code/gnr/results/active_folder.tmp"
 
 if os.path.isfile(folder_file):
     with open(folder_file, "r") as f:
         path = f.read().strip()
+    path_folders = path.split('/')
+    new_path = '/'.join(path_folders[11:])
+    print(f"\nActive folder is {new_path}")
     os.chdir(path)
-    with open(job_file, "r") as f:
-        last_job = int(f.read().strip())
+    if os.path.isfile(job_file):
+        with open(job_file, "r") as f:
+            last_job = int(f.read().strip())
+            print(f"Last submitted job is {last_job}")
+    else:
+        print(f"{job_file} does not exist")
+        exit()
 else:
+    user_input = input("There is no active folder. Continue? (y/n): ")
+    if user_input.lower() == 'n':
+        exit()
     last_job = job_min
-    cwd = os.getcwd()
+    path = os.getcwd()
+    path_folders = path.split('/')
+    new_path = '/'.join(path_folders[8:])
+    print(f"\nActive folder is {new_path}")
     with open(folder_file, "w") as f:
-        f.write(cwd)
+        f.write(path)
 
 for queue, maxsubmit in zip(queues, maxsubmits):
 
@@ -30,9 +45,35 @@ for queue, maxsubmit in zip(queues, maxsubmits):
     print(f"{num_jobs_in_queue} jobs in queue")
     available_slots = maxsubmit - num_jobs_in_queue
     print(f"{available_slots} slots available")
-    num_jobs_to_submit = min(available_slots, job_max - last_job)
 
-    if num_jobs_to_submit > 0:
+    while available_slots > 0:
+
+        if last_job == job_max:
+            os.remove(job_file)
+            folder_name = os.path.basename(path)
+            folder_index = folders.index(folder_name)
+            changed_dir = False
+            for folder in folders[folder_index + 1:]:
+                next_folder = os.path.join('../', folder)
+                if os.path.isdir(next_folder):
+                    os.chdir(next_folder)
+                    changed_dir = True
+                    last_job = job_min
+                    path = os.getcwd()
+                    path_folders = path.split('/')
+                    new_path = '/'.join(path_folders[11:])
+                    print(f"Active folder is now {new_path}")
+                    with open(job_file, 'w') as f:
+                        f.write(str(job_min))
+                    with open(folder_file, 'w') as f:
+                        f.write(path)
+                    break
+            if not changed_dir:
+                print("All jobs completed")
+                os.remove(folder_file)
+                exit()
+
+        num_jobs_to_submit = min(available_slots, job_max - last_job)
         job_name = f"{queue}-{os.getcwd().split('/')[-1]}"
         first_job = last_job + 1
         last_job = last_job + num_jobs_to_submit
@@ -44,17 +85,15 @@ for queue, maxsubmit in zip(queues, maxsubmits):
                         "--array", array,
                         "job.sh"])
         print(f"with jobs {first_job} to {last_job}")
-    else:
-        print("No jobs to submit")
+        #available_slots = available_slots - num_jobs_to_submit
 
-    available_slots = available_slots - num_jobs_to_submit
-    print(f"{available_slots} slots still available")
+        output = subprocess.check_output(f"squeue -t RUNNING,PENDING -r -o '%j' | grep -E '^{queue}' | wc -l", shell=True)
+        num_jobs_in_queue = int(output.decode().strip())
+        print(f"{num_jobs_in_queue} jobs in queue")
+        available_slots = maxsubmit - num_jobs_in_queue
+        print(f"{available_slots} slots available")
 
 print("")
 
-if last_job == job_max:
-    os.remove(job_file)
-    os.remove(folder_file)
-else:
-    with open(job_file, "w") as f:
-        f.write(str(last_job))
+with open(job_file, "w") as f:
+    f.write(str(last_job))

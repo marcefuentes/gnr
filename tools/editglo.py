670b5adaf734618997417039345a27ac9eff8cbe
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-05-25 13:58:02 +0200

    Update

--- tools/editglo.py
+++ tools/editglo.py
@@ -8,7 +8,7 @@ pattern = '*.glo'
 abs_folder_path = os.path.abspath(os.getcwd())
 parent_folder = os.path.basename(os.path.dirname(abs_folder_path))
 folder_name = os.path.basename(abs_folder_path)
-decimal_part = parent_folder[-2:]
+decimal_part = parent_folder[-3:]
 given = float(decimal_part) / 100
 
 for line in fileinput.input(files=[os.path.join(abs_folder_path, f) for f in os.listdir(abs_folder_path) if f.endswith('.glo')], inplace=True, backup='',):

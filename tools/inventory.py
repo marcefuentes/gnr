f09116b2f7ed87262900ad157f29b8bdcb57a750
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-07-14 11:00:21 +0200

    Update

--- tools/inventory.py
+++ tools/inventory.py
@@ -35,35 +35,41 @@ for mechanism in mechanisms:
         if len(input_files) == 0:
             print(f"{red}no {input_file_extension[1:]} files{reset_format}")
             continue
+        f_smaller_nlines = 0
         f_equal_nlines = 0
         f_larger_nlines = 0
-        f_smaller_nlines = 0
         for f in os.listdir(given_path):
             if f.endswith(output_file_extension):
                 output_file = os.path.join(given_path, f)
                 with open(output_file, "r") as output:
                     lines = output.readlines()
-                    if len(lines) == nlines:
+                    if len(lines) < nlines:
+                        f_smaller_nlines += 1
+                    elif len(lines) == nlines:
                         f_equal_nlines += 1
                     elif len(lines) > nlines:
                         f_larger_nlines += 1
-                    elif len(lines) < nlines:
-                        f_smaller_nlines += 1
         if f_equal_nlines == len(input_files):
             print(f"{yellow}ok{reset_format}")
         else:
-            if f_equal_nlines + f_larger_nlines + f_smaller_nlines == 0:
-                print(f"{red}no{reset_format} {output_file_extension[1:]} files")
-            else:
-                if f_larger_nlines:
-                    print(f"{red}{f_larger_nlines}{reset_format} {output_file_extension[1:]} files with > {nlines} lines")
-                if f_smaller_nlines:
-                    if f_larger_nlines:
-                        print(f"{' ' * (len(mechanism) + len(given) + 2)}{red}", end = "")
-                    print(f"{red}{f_smaller_nlines}{reset_format} incomplete {output_file_extension[1:]} files")
-                missing = len(input_files) - f_equal_nlines - f_larger_nlines - f_smaller_nlines
-                if missing:
-                    if f_larger_nlines or f_smaller_nlines:
-                        print(f"{' ' * (len(mechanism) + len(given) + 2)}{red}", end = "")
-                    print(f"{red}{missing}{reset_format} missing {output_file_extension[1:]} files")
+            first = True
+            if f_equal_nlines:
+                print(f"{red}{f_equal_nlines}{reset_format} finished", end = "")
+                first = False
+            if f_smaller_nlines:
+                if not first:
+                    print(", ", end = "")
+                print(f"{red}{f_smaller_nlines}{reset_format} incomplete", end = "")
+                first = False
+            notstarted = len(input_files) - f_smaller_nlines - f_equal_nlines - f_larger_nlines
+            if notstarted:
+                if not first:
+                    print(", ", end = "")
+                print(f"{red}{notstarted}{reset_format} not started", end = "")
+                first = False
+            if f_larger_nlines:
+                if not first:
+                    print(", ", end = "")
+                print(f"{red}{f_larger_nlines}{reset_format} > {nlines} lines", end = "")
+            print()
             

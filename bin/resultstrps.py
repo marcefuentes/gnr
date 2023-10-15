c66dd320126a7a364220d0839a3512e189fe2a0e
Marcelino Fuentes <marcelinofuentes@gmail.com>
2023-10-15 20:59:18 +0200

    Update

--- bin/resultstrps.py
+++ bin/resultstrps.py
@@ -36,7 +36,7 @@ def init(artists):
                         R = my.fitness(y, y, given, alpha, rho)
                         P = my.fitness(x, x, given, alpha, rho)
                         S = my.fitness(x, y, given, alpha, rho)
-                        condition = (T > R) & (R > P) & (P < S) & (T + S > 2.0*R) & (P == 0.0)
+                        condition = (T > R) & (R > P) & (P < S) & (T + S < 2.0*R) & (P == 0.0)
                         if condition:
                             artists[i, j, k, l].set_color("white")
                         else:
